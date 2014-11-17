7e1e80a58e309d6050dcd4d2826bebcbfee51f11
Dain Nilsson <dain@yubico.com>
2014-11-17 16:08:58 +0100

    Fix typo.

--- u2flib-server-core/src/main/java/com/yubico/u2f/exceptions/NoDevicesRegisteredException.java
+++ u2flib-server-core/src/main/java/com/yubico/u2f/exceptions/NoDevicesRegisteredException.java
@@ -2,6 +2,6 @@ package com.yubico.u2f.exceptions;
 
 public class NoDevicesRegisteredException extends U2fException {
     public NoDevicesRegisteredException() {
-        super("The user had no U2F devices registered");
+        super("The user has no U2F devices registered");
     }
 }
