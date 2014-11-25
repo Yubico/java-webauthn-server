9639d52356fe7eaf91e509480dcce42f19764f03
Henrik Stråth <henrik.strath@gmail.com>
2014-11-25 16:44:51 +0100

    more demo server polish

--- u2flib-server-demo/src/main/java/demo/Resource.java
+++ u2flib-server-demo/src/main/java/demo/Resource.java
@@ -14,8 +14,6 @@ import io.dropwizard.views.View;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.*;
 
@@ -26,8 +24,7 @@ public class Resource {
     public static final String SERVER_ADDRESS = "http://example.com:8080";
     public static final String NAVIGATION_MENU = "<h2>Navigation</h2><ul><li><a href='registerIndex'>Register</a></li><li><a href='loginIndex'>Login</a></li></ul>.";
 
-    // In production, you want to store DeviceRegistrations persistently (e.g. in a database).
-    private final Map<String, String> storage = new HashMap<String, String>();
+    private final Map<String, String> requestStorage = new HashMap<String, String>();
     private final Multimap<String, String> userStorage = ArrayListMultimap.create();
     private final U2F u2f = new U2F();
 
@@ -48,7 +45,7 @@ public class Resource {
     @GET
     public View startRegistration(@QueryParam("username") String username) {
         RegisterRequestData registerRequestData = u2f.startRegistration(SERVER_ADDRESS, getRegistrations(username));
-        storage.put(registerRequestData.getRequestId(), registerRequestData.toJson());
+        requestStorage.put(registerRequestData.getRequestId(), registerRequestData.toJson());
         return new RegistrationView(registerRequestData.toJson(), username);
     }
 
@@ -57,10 +54,10 @@ public class Resource {
     public String finishRegistration(@FormParam("tokenResponse") String response, @FormParam("username") String username)
             throws U2fException {
         RegisterResponse registerResponse = RegisterResponse.fromJson(response);
-        RegisterRequestData registerRequestData = RegisterRequestData.fromJson(storage.get(registerResponse.getRequestId()));
+        RegisterRequestData registerRequestData = RegisterRequestData.fromJson(requestStorage.get(registerResponse.getRequestId()));
         DeviceRegistration registration = u2f.finishRegistration(registerRequestData, registerResponse);
         addRegistration(username, registration);
-        storage.remove(registerResponse.getRequestId());
+        requestStorage.remove(registerResponse.getRequestId());
         return "<p>Successfully registered device:</p><pre>" +
                 registration +
                 "</pre>" + NAVIGATION_MENU;
@@ -70,7 +67,7 @@ public class Resource {
     @GET
     public View startAuthentication(@QueryParam("username") String username) throws U2fException {
         AuthenticateRequestData authenticateRequestData = u2f.startAuthentication(SERVER_ADDRESS, getRegistrations(username));
-        storage.put(authenticateRequestData.getRequestId(), authenticateRequestData.toJson());
+        requestStorage.put(authenticateRequestData.getRequestId(), authenticateRequestData.toJson());
         return new AuthenticationView(authenticateRequestData.toJson(), username);
     }
 
@@ -79,8 +76,8 @@ public class Resource {
     public String finishAuthentication(@FormParam("tokenResponse") String response,
                                        @FormParam("username") String username) throws U2fException {
         AuthenticateResponse authenticateResponse = AuthenticateResponse.fromJson(response);
-        AuthenticateRequestData authenticateRequest = AuthenticateRequestData.fromJson(storage.get(authenticateResponse.getRequestId()));
-        storage.remove(authenticateResponse.getRequestId());
+        AuthenticateRequestData authenticateRequest = AuthenticateRequestData.fromJson(requestStorage.get(authenticateResponse.getRequestId()));
+        requestStorage.remove(authenticateResponse.getRequestId());
         u2f.finishAuthentication(authenticateRequest, authenticateResponse, getRegistrations(username));
         return "<p>Successfully authenticated!<p>" + NAVIGATION_MENU;
     }
