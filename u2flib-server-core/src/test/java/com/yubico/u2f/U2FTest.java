7cafdcf5fc7ff2b47041a573b3a17787a0dfbb70
Dain Nilsson <dain@yubico.com>
2015-01-22 15:05:25 +0100

    Added test for invalid facet.

--- u2flib-server-core/src/test/java/com/yubico/u2f/U2FTest.java
+++ u2flib-server-core/src/test/java/com/yubico/u2f/U2FTest.java
@@ -1,11 +1,13 @@
 package com.yubico.u2f;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.yubico.u2f.data.DeviceRegistration;
 import com.yubico.u2f.data.messages.AuthenticateRequest;
 import com.yubico.u2f.data.messages.AuthenticateRequestData;
 import com.yubico.u2f.data.messages.AuthenticateResponse;
 import com.yubico.u2f.exceptions.DeviceCompromisedException;
+import com.yubico.u2f.exceptions.InvalidFacetException;
 import com.yubico.u2f.exceptions.NoEligableDevicesException;
 import org.junit.Test;
 
@@ -46,5 +48,21 @@ public class U2FTest {
 
         deviceRegistration.markCompromised();
         u2f.finishAuthentication(authenticateRequest, tokenResponse, ImmutableList.of(deviceRegistration));
+    }
+
+    @Test(expected = InvalidFacetException.class)
+    public void finishAuthentication_invalidFacet() throws Exception {
+        DeviceRegistration deviceRegistration = new DeviceRegistration(KEY_HANDLE_BASE64, USER_PUBLIC_KEY_AUTHENTICATE_HEX, ATTESTATION_CERTIFICATE, 0);
+
+        AuthenticateRequest request = new AuthenticateRequest(SERVER_CHALLENGE_SIGN_BASE64,
+                APP_ID_SIGN, KEY_HANDLE_BASE64);
+
+        AuthenticateResponse tokenResponse = new AuthenticateResponse(CLIENT_DATA_AUTHENTICATE_BASE64,
+                SIGN_RESPONSE_DATA_BASE64, KEY_HANDLE_BASE64);
+
+        AuthenticateRequestData authenticateRequest = mock(AuthenticateRequestData.class);
+        when(authenticateRequest.getAuthenticateRequest(tokenResponse)).thenReturn(request);
+
+        u2f.finishAuthentication(authenticateRequest, tokenResponse, ImmutableList.of(deviceRegistration), ImmutableSet.of("https://wrongfacet.com"));
     }
 }
