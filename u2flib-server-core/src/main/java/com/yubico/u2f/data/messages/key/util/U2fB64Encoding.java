f768bc2b65604bec856b2af514a881a8b09e83ea
Jeroen Hoek <jeroen.hoek@lable.nl>
2016-01-25 14:34:14 +0100

    Decode Base64 data with padding as well
    By default Base64 encoded data is emitted by this library with the
    padding ('=') omitted, but there is no guarantee that inbound data omits
    the padding character as well.

--- u2flib-server-core/src/main/java/com/yubico/u2f/data/messages/key/util/U2fB64Encoding.java
+++ u2flib-server-core/src/main/java/com/yubico/u2f/data/messages/key/util/U2fB64Encoding.java
@@ -3,13 +3,14 @@ package com.yubico.u2f.data.messages.key.util;
 import com.google.common.io.BaseEncoding;
 
 public class U2fB64Encoding {
-    private final static BaseEncoding U2F_ENCODING = BaseEncoding.base64Url().omitPadding();
+    private final static BaseEncoding BASE64_ENCODER = BaseEncoding.base64Url().omitPadding();
+    private final static BaseEncoding BASE64_DECODER = BaseEncoding.base64Url();
 
     public static String encode(byte[] decoded) {
-        return U2F_ENCODING.encode(decoded);
+        return BASE64_ENCODER.encode(decoded);
     }
 
     public static byte[] decode(String encoded) {
-        return U2F_ENCODING.decode(encoded);
+        return BASE64_DECODER.decode(encoded);
     }
 }
