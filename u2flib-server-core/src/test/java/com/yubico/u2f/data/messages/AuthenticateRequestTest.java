29cb1719a94a0bfd9e2c6a3042a8d98e0e4c40e6
Emil Lundberg <emil@yubico.com>
2017-08-11 19:18:33 +0200

    Add test for AuthenticateRequest.getRequestId()

--- u2flib-server-core/src/test/java/com/yubico/u2f/data/messages/AuthenticateRequestTest.java
+++ u2flib-server-core/src/test/java/com/yubico/u2f/data/messages/AuthenticateRequestTest.java
@@ -15,6 +15,7 @@ public class AuthenticateRequestTest {
     public void testGetters() throws Exception {
         AuthenticateRequest authenticateRequest = AuthenticateRequest.builder().challenge(SERVER_CHALLENGE_SIGN_BASE64).appId(APP_ID_SIGN).keyHandle(KEY_HANDLE_BASE64).build();
         assertEquals(SERVER_CHALLENGE_SIGN_BASE64, authenticateRequest.getChallenge());
+        assertEquals(SERVER_CHALLENGE_SIGN_BASE64, authenticateRequest.getRequestId());
         assertEquals(APP_ID_SIGN, authenticateRequest.getAppId());
         assertEquals(KEY_HANDLE_BASE64, authenticateRequest.getKeyHandle());
     }
