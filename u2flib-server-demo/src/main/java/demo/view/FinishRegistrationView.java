970ca25d52fac200938157b17b1aa21230299b29
Emil Lundberg <emil@yubico.com>
2017-08-14 14:04:10 +0200

    Use @lombok.Getter in FinishRegistrationView

--- u2flib-server-demo/src/main/java/demo/view/FinishRegistrationView.java
+++ u2flib-server-demo/src/main/java/demo/view/FinishRegistrationView.java
@@ -3,7 +3,9 @@ package demo.view;
 import com.yubico.u2f.attestation.Attestation;
 import com.yubico.u2f.data.DeviceRegistration;
 import io.dropwizard.views.View;
+import lombok.Getter;
 
+@Getter
 public class FinishRegistrationView extends View {
 
     private Attestation attestation;
@@ -13,14 +15,6 @@ public class FinishRegistrationView extends View {
         super("finishRegistration.ftl");
         this.attestation = attestation;
         this.registration = registration;
-    }
-
-    public Attestation getAttestation() {
-        return attestation;
-    }
-
-    public DeviceRegistration getRegistration() {
-        return registration;
     }
 
 }
