57445acfbb564b23ee2b211f6286847b6b5b4584
Emil Lundberg <lundberg.emil@gmail.com>
2017-08-31 20:13:54 +0200

    Add BinaryUtil.fromHex

--- u2flib-server-core/src/test/scala/com/yubico/webauthn/util/BinaryUtil.scala
+++ u2flib-server-core/src/test/scala/com/yubico/webauthn/util/BinaryUtil.scala
@@ -1,5 +1,7 @@
 package com.yubico.webauthn.util
 
+import scala.util.Try
+
 object BinaryUtil {
 
   /**
@@ -7,5 +9,25 @@ object BinaryUtil {
     * @return The `bytes` encoded as lowercase hexadecimal digits
     */
   def toHex(bytes: Seq[Byte]): String = bytes map (_.toInt.toHexString) mkString ""
+
+  /**
+    * @param hex String of hexadecimal digits to decode as bytes.
+    * @return
+    *         - [[IllegalArgumentException]] if the length of `hex` is not even.
+    *         - [[NumberFormatException]] if `hex` contains characters that are not hexadecimal digits.
+    *         - The decoded bytes otherwise.
+    */
+  def fromHex(hex: String): Try[Vector[Byte]] = Try(
+    hex.length % 2 match {
+      case 0 =>
+        hex
+          .grouped(2)
+          .map { hexDigits => Integer.parseInt(hexDigits, 16).toByte }
+          .toVector
+
+      case _ =>
+        throw new IllegalArgumentException(s"Hex string must be of even length, was: ${hex.length}")
+    }
+  )
 
 }
