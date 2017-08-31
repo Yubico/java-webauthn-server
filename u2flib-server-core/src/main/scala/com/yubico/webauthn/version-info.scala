7f2f9be98210441a55ba8fcf629fee72bc807752
Emil Lundberg <lundberg.emil@gmail.com>
2017-08-31 16:26:27 +0200

    Add class com.yubico.webauthn.VersionInfo

---
+++ u2flib-server-core/src/main/scala/com/yubico/webauthn/version-info.scala
@@ -0,0 +1,39 @@
+package com.yubico.webauthn
+
+import java.net.URL
+import java.time.LocalDate
+
+sealed trait DocumentStatus
+case object WorkingDraft extends DocumentStatus
+
+/**
+  * Reference to a particular version of a specification document
+  *
+  * @param url Address to this version of the specification
+  * @param latestVersionUrl Address to the latest version of this specification
+  * @param status An object indicating the status of the specification document
+  * @param releaseDate The release date of the specification document
+  */
+case class Specification(
+  url: URL,
+  latestVersionUrl: URL,
+  status: DocumentStatus,
+  releaseDate: LocalDate
+)
+
+/**
+  * Contains version information for the com.yubico.webauthn package
+  */
+object VersionInfo {
+
+  /**
+    * Represents the specification this implementation is based on
+    */
+  val specification = Specification(
+    url = new URL("https://www.w3.org/TR/2017/WD-webauthn-20170811/"),
+    latestVersionUrl = new URL("https://www.w3.org/TR/webauthn/"),
+    status = WorkingDraft,
+    releaseDate = LocalDate.parse("2017-08-11")
+  )
+
+}
