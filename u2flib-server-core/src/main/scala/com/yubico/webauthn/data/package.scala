0932582040702ce8275d5993f549c2ccc1834930
Emil Lundberg <lundberg.emil@gmail.com>
2017-08-31 19:15:04 +0200

    Change type alias AuthenticationExtensions from Map[String,Any] to JsonNode

--- u2flib-server-core/src/main/scala/com/yubico/webauthn/data/package.scala
+++ u2flib-server-core/src/main/scala/com/yubico/webauthn/data/package.scala
@@ -1,12 +1,14 @@
 package com.yubico.webauthn
 
+import com.fasterxml.jackson.databind.JsonNode
+
 package object data {
 
   /** An immutable array of bytes. */
   type ArrayBuffer = Vector[Byte]
 
   /** Container for extension values in [[PublicKeyCredential]]. */
-  type AuthenticationExtensions = Map[String, Any]
+  type AuthenticationExtensions = JsonNode
 
   /** A URL-safe base64 encoded array of bytes. */
   type Base64UrlString = String
