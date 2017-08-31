7f0fd4bf12968125087c1a8b024a801f24b7af06
Emil Lundberg <lundberg.emil@gmail.com>
2017-08-31 20:13:42 +0200

    Add Scaladoc to BinaryUtil.toHex

--- u2flib-server-core/src/test/scala/com/yubico/webauthn/util/BinaryUtil.scala
+++ u2flib-server-core/src/test/scala/com/yubico/webauthn/util/BinaryUtil.scala
@@ -2,6 +2,10 @@ package com.yubico.webauthn.util
 
 object BinaryUtil {
 
+  /**
+    * @param bytes Bytes to encode
+    * @return The `bytes` encoded as lowercase hexadecimal digits
+    */
   def toHex(bytes: Seq[Byte]): String = bytes map (_.toInt.toHexString) mkString ""
 
 }
