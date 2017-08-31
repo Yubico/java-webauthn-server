9a7d3c54ffb602c13547832b02a818b6b4993ab8
Emil Lundberg <lundberg.emil@gmail.com>
2017-08-31 15:19:41 +0200

    Set up ScalaTest

---
+++ u2flib-server-core/src/test/scala/com/yubico/webauthn/rp/RpOperationsSpec.scala
@@ -0,0 +1,11 @@
+/* Copyright 2017 Yubico */
+
+package com.yubico.webauthn.rp
+
+import org.scalatest.FunSpec
+import org.scalatest.Matchers
+
+
+class RpOperationsSpec extends FunSpec with Matchers {
+
+}
