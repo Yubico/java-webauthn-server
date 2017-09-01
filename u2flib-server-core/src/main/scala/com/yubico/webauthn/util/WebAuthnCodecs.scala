561df6cc5e3662cb3e86b4214f004706d102864d
Emil Lundberg <emil@yubico.com>
2017-09-01 19:53:04 +0200

    Use URL safe Base64 encoding in WebAuthnCodecs

--- u2flib-server-core/src/main/scala/com/yubico/webauthn/util/WebAuthnCodecs.scala
+++ u2flib-server-core/src/main/scala/com/yubico/webauthn/util/WebAuthnCodecs.scala
@@ -7,8 +7,8 @@ import com.fasterxml.jackson.dataformat.cbor.CBORFactory
 
 object WebAuthnCodecs {
 
-  def cbor: ObjectMapper = new ObjectMapper(new CBORFactory())
+  def cbor: ObjectMapper = new ObjectMapper(new CBORFactory()).setBase64Variant(Base64Variants.MODIFIED_FOR_URL)
 
-  def json: ObjectMapper = new ObjectMapper()
+  def json: ObjectMapper = new ObjectMapper().setBase64Variant(Base64Variants.MODIFIED_FOR_URL)
 
 }
