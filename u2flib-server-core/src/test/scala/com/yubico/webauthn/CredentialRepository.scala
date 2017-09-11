11d8c5ee0c7683de32f7a1e9cd8852f87de6ecb1
Emil Lundberg <emil@yubico.com>
2017-09-11 09:54:03 +0200

    Add default implementation of CredentialRepository.lookup(ArrayBuffer)

--- u2flib-server-core/src/test/scala/com/yubico/webauthn/CredentialRepository.scala
+++ u2flib-server-core/src/test/scala/com/yubico/webauthn/CredentialRepository.scala
@@ -3,13 +3,14 @@ package com.yubico.webauthn
 import java.security.PublicKey
 import java.util.Optional
 
+import com.yubico.u2f.data.messages.key.util.U2fB64Encoding
 import com.yubico.webauthn.data.ArrayBuffer
 import com.yubico.webauthn.data.Base64UrlString
 
 
 trait CredentialRepository {
 
-  def lookup(credentialId: ArrayBuffer): Optional[PublicKey]
+  def lookup(credentialId: ArrayBuffer): Optional[PublicKey] = lookup(U2fB64Encoding.encode(credentialId.toArray))
   def lookup(credentialId: Base64UrlString): Optional[PublicKey]
 
 }
