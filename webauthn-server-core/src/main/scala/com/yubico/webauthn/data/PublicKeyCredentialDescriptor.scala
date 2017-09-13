c1ba243b905b7cc4f914968cb1d1af5b15ab9627
Emil Lundberg <emil@yubico.com>
2017-09-13 16:14:52 +0200

    Configure JSON serialization of PublicKeyCredentialDescriptor

--- webauthn-server-core/src/main/scala/com/yubico/webauthn/data/PublicKeyCredentialDescriptor.scala
+++ webauthn-server-core/src/main/scala/com/yubico/webauthn/data/PublicKeyCredentialDescriptor.scala
@@ -2,6 +2,8 @@ package com.yubico.webauthn.data
 
 import java.util.Optional
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.yubico.scala.util.JavaConverters._
 import com.yubico.u2f.data.messages.key.util.U2fB64Encoding
 
@@ -16,17 +18,23 @@ case class PublicKeyCredentialDescriptor(
   /**
     * The type of the credential the caller is referring to.
     */
+  @JsonIgnore
   `type`: PublicKeyCredentialType = PublicKey,
 
   /**
     * The identifier of the credential that the caller is referring to.
     */
+  @JsonIgnore
   id: ArrayBuffer,
 
   transports: Optional[Vector[AuthenticatorTransport]] = None.asJava
 
 ) {
 
+  @JsonProperty("id")
   def idBase64: String = U2fB64Encoding.encode(id.toArray)
+
+  @JsonProperty("type")
+  def typeJson: String = `type`.id
 
 }
