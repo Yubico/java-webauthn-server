9a71d1788ded616712e8606c74302b6851c4e681
Emil Lundberg <emil@yubico.com>
2018-03-22 15:26:07 +0100

    Make TokenBindingStatus objects case objects

--- webauthn-server-core/src/main/scala/com/yubico/webauthn/data/TokenBindingStatus.scala
+++ webauthn-server-core/src/main/scala/com/yubico/webauthn/data/TokenBindingStatus.scala
@@ -9,6 +9,6 @@ sealed trait TokenBindingStatus {
   def jsonValue: String
   def toJson: String = jsonValue
 }
-object NotSupported extends TokenBindingStatus { override val jsonValue = "not-supported" }
-object Present extends TokenBindingStatus      { override val jsonValue = "present"       }
-object Supported extends TokenBindingStatus    { override val jsonValue = "supported"     }
+case object NotSupported extends TokenBindingStatus { override val jsonValue = "not-supported" }
+case object Present extends TokenBindingStatus      { override val jsonValue = "present"       }
+case object Supported extends TokenBindingStatus    { override val jsonValue = "supported"     }
