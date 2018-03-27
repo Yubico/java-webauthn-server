d6703217ed7c0dd91182c3366ea239cac922ce4d
Emil Lundberg <emil@yubico.com>
2018-03-27 11:58:41 +0200

    Add custom JSON serializer for UserVerificationRequirement

--- webauthn-server-core/src/main/scala/com/yubico/webauthn/data/UserVerificationRequirement.scala
+++ webauthn-server-core/src/main/scala/com/yubico/webauthn/data/UserVerificationRequirement.scala
@@ -1,5 +1,22 @@
 package com.yubico.webauthn.data
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+
+private class UVRSerializer extends JsonSerializer[UserVerificationRequirement] {
+  override def serialize(t: UserVerificationRequirement, jsonGenerator: JsonGenerator, serializerProvider: SerializerProvider): Unit =
+    jsonGenerator.writeString(
+      t match {
+        case Discouraged => "discouraged"
+        case Preferred => "preferred"
+        case Required => "required"
+      }
+    )
+}
+
+@JsonSerialize(using = classOf[UVRSerializer])
 sealed trait UserVerificationRequirement
 case object Discouraged extends UserVerificationRequirement
 case object Preferred extends UserVerificationRequirement
