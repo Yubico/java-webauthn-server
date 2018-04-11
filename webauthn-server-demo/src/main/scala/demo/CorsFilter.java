a1ce064381faf927937942210361285831497c2f
Emil Lundberg <emil@yubico.com>
2018-04-11 16:11:50 +0200

    Make CorsFilter use the same origins as the WebAuthn server

--- webauthn-server-demo/src/main/scala/demo/CorsFilter.java
+++ webauthn-server-demo/src/main/scala/demo/CorsFilter.java
@@ -4,13 +4,19 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 
+import demo.webauthn.Config;
 import java.io.IOException;
 
 public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:8080");
+        String origin = requestContext.getHeaderString("origin");
+        Config.getOrigins().stream()
+            .filter(allowedOrigin -> allowedOrigin.equals(origin))
+            .forEach(allowedOrigin ->
+                responseContext.getHeaders().add("Access-Control-Allow-Origin", allowedOrigin)
+            );
     }
 
 }
