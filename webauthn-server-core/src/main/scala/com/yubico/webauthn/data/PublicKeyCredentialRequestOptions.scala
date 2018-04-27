48e1524ab8483c83c3cb4e9f2c8d99d6f74f846a
Emil Lundberg <emil@yubico.com>
2018-04-27 16:14:33 +0200

    Update PublicKeyCredentialRequestOptions structure and docs

--- webauthn-server-core/src/main/scala/com/yubico/webauthn/data/PublicKeyCredentialRequestOptions.scala
+++ webauthn-server-core/src/main/scala/com/yubico/webauthn/data/PublicKeyCredentialRequestOptions.scala
@@ -9,17 +9,28 @@ import com.yubico.u2f.data.messages.key.util.U2fB64Encoding
 
 
 /**
-  * The PublicKeyCredentialRequestOptions dictionary supplies get() with the data it needs to generate an assertion.
+  * The PublicKeyCredentialRequestOptions dictionary supplies get() with the
+  * data it needs to generate an assertion.
   *
-  * Its `challenge` member must be present, while its other members are optional.
+  * Its `challenge` member must be present, while its other members are
+  * optional.
   */
 case class PublicKeyCredentialRequestOptions(
 
   /**
-    * A challenge that the selected authenticator signs, along with other data, when producing an authentication assertion.
+    * A challenge that the selected authenticator signs, along with other data,
+    * when producing an authentication assertion.
     */
   @JsonIgnore
   challenge: ArrayBuffer,
+
+  /**
+    * Specifies a time, in milliseconds, that the caller is willing to wait for
+    * the call to complete.
+    *
+    * This is treated as a hint, and MAY be overridden by the platform.
+    */
+  timeout: Optional[Long] = None.asJava,
 
   /**
     * Specifies the relying party identifier claimed by the caller.
@@ -29,19 +40,26 @@ case class PublicKeyCredentialRequestOptions(
   rpId: Optional[String] = None.asJava,
 
   /**
-    * A list of public key credentials acceptable to the caller, in descending order of the caller’s preference.
+    * A list of public key credentials acceptable to the caller, in descending
+    * order of the caller’s preference.
     */
   allowCredentials: Optional[java.util.List[PublicKeyCredentialDescriptor]] = None.asJava,
 
   /**
-    * This member describes the Relying Party's requirements regarding user verification for the get() operation. Eligible authenticators are filtered to only those capable of satisfying this requirement.
+    * Describes the Relying Party's requirements regarding user verification
+    * for the get() operation.
+    *
+    * Eligible authenticators are filtered to only those capable of satisfying
+    * this requirement.
     */
   userVerification: UserVerificationRequirement = Preferred,
 
   /**
-    * Additional parameters requesting additional processing by the client and authenticator.
+    * Additional parameters requesting additional processing by the client and
+    * authenticator.
     *
-    * For example, if transaction confirmation is sought from the user, then the prompt string might be included as an extension.
+    * For example, if transaction confirmation is sought from the user, then
+    * the prompt string might be included as an extension.
     */
   extensions: Optional[AuthenticationExtensions] = None.asJava
 
