35377acb2fffea0503875e6ade7f9ac32fdc013a
Emil Lundberg <emil@yubico.com>
2018-05-04 17:22:49 +0200

    Remove unused imports

--- webauthn-server-core/src/main/scala/com/yubico/webauthn/CredentialRepository.scala
+++ webauthn-server-core/src/main/scala/com/yubico/webauthn/CredentialRepository.scala
@@ -2,9 +2,6 @@ package com.yubico.webauthn
 
 import java.util.Optional
 
-import com.yubico.scala.util.JavaConverters._
-import com.yubico.u2f.data.messages.key.util.U2fB64Encoding
-import com.yubico.webauthn.data.ArrayBuffer
 import com.yubico.webauthn.data.Base64UrlString
 
 
