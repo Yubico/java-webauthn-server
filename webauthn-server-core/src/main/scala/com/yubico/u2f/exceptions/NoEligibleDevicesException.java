47bc335185e626d5ab1f2f102aca93e7c7bde8ee
Emil Lundberg <emil@yubico.com>
2018-05-07 13:17:56 +0200

    Remove class NoEligableDevicesException

--- webauthn-server-core/src/main/scala/com/yubico/u2f/exceptions/NoEligibleDevicesException.java
+++ webauthn-server-core/src/main/scala/com/yubico/u2f/exceptions/NoEligibleDevicesException.java
@@ -1,16 +1,23 @@
 package com.yubico.u2f.exceptions;
 
+import com.google.common.collect.ImmutableList;
 import com.yubico.u2f.data.DeviceRegistration;
+import java.util.List;
 
-@SuppressWarnings("deprecation")
-public class NoEligibleDevicesException extends NoEligableDevicesException {
+public class NoEligibleDevicesException extends U2fAuthenticationException {
+    private final List<DeviceRegistration> devices;
 
     public NoEligibleDevicesException(Iterable<? extends DeviceRegistration> devices, String message, Throwable cause) {
-        super(devices, message, cause);
+        super(message, cause);
+        this.devices = ImmutableList.copyOf(devices);
     }
 
     public NoEligibleDevicesException(Iterable<? extends DeviceRegistration> devices, String message) {
-        super(devices, message);
+        super(message);
+        this.devices = ImmutableList.copyOf(devices);
     }
 
+    public boolean hasDevices() {
+        return !devices.isEmpty();
+    }
 }
