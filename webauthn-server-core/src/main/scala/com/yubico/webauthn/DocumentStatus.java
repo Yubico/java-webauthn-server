4e28ef6409752c78193a02282e30a3f76730ddea
Emil Lundberg <emil@yubico.com>
2018-05-07 14:35:53 +0200

    Remove unused import

--- webauthn-server-core/src/main/scala/com/yubico/webauthn/DocumentStatus.java
+++ webauthn-server-core/src/main/scala/com/yubico/webauthn/DocumentStatus.java
@@ -1,7 +1,6 @@
 package com.yubico.webauthn;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.io.IOException;
