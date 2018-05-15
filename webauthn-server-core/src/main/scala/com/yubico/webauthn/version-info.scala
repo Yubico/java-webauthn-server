c31eb9bf823aed62e32a5b15cb23c2f5bcad76ba
Emil Lundberg <emil@yubico.com>
2018-05-15 18:22:24 +0200

    Show implementation version in VersionInfo

--- webauthn-server-core/src/main/scala/com/yubico/webauthn/version-info.scala
+++ webauthn-server-core/src/main/scala/com/yubico/webauthn/version-info.scala
@@ -2,6 +2,7 @@ package com.yubico.webauthn
 
 import java.net.URL
 import java.time.LocalDate
+import java.util.Optional
 
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.JsonSerializer
@@ -14,6 +15,17 @@ private class LocalDateJsonSerializer extends JsonSerializer[LocalDate] {
     jsonGenerator.writeString(t.toString)
   }
 }
+
+/**
+  * Description of this version of this library
+  *
+  * @param version The version number of this release of the library.
+  * @param sourceCodeUrl Address to where the source code for this library can be found.
+  */
+case class Implementation(
+  version: Optional[String],
+  sourceCodeUrl: URL
+)
 
 /**
   * Reference to a particular version of a specification document.
@@ -47,5 +59,27 @@ object VersionInfo {
     status = DocumentStatus.CANDIDATE_RECOMMENDATION,
     releaseDate = LocalDate.parse("2018-03-20")
   )
+
+  /**
+    * Represents the specification this implementation is based on
+    */
+  val implementation = Implementation(
+    version = findImplementationVersionInManifest(),
+    sourceCodeUrl = new URL("https://github.com/Yubico/java-webauthn-server")
+  )
+
+  private def findImplementationVersionInManifest(): Optional[String] = {
+    val resources = getClass.getClassLoader.getResources("META-INF/MANIFEST.MF")
+
+    while (resources.hasMoreElements) {
+      val resource = resources.nextElement()
+      val manifest = new java.util.jar.Manifest(resource.openStream())
+      if (manifest.getMainAttributes.getValue("Implementation-Id") == "java-webauthn-server") {
+        return Optional.ofNullable(manifest.getMainAttributes.getValue("Implementation-Version"))
+      }
+    }
+
+    return Optional.empty()
+  }
 
 }
