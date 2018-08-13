064122f3468471b4725ceedcde87a1d121bc53e3
Emil Lundberg <emil@yubico.com>
2018-08-13 16:45:49 +0200

    Catch IOException inside X5cAttestationStatementVerifier

--- webauthn-server-core/src/main/java/com/yubico/webauthn/impl/X5cAttestationStatementVerifier.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/impl/X5cAttestationStatementVerifier.java
@@ -13,11 +13,11 @@ import java.util.Optional;
 
 public interface X5cAttestationStatementVerifier {
 
-    default Optional<X509Certificate> getX5cAttestationCertificate(AttestationObject attestationObject) throws IOException, CertificateException {
+    default Optional<X509Certificate> getX5cAttestationCertificate(AttestationObject attestationObject) throws CertificateException {
         return getAttestationTrustPath(attestationObject).flatMap(certs -> certs.stream().findFirst());
     }
 
-    default Optional<List<X509Certificate>> getAttestationTrustPath(AttestationObject attestationObject) throws IOException, CertificateException {
+    default Optional<List<X509Certificate>> getAttestationTrustPath(AttestationObject attestationObject) throws CertificateException {
         JsonNode x5cNode = attestationObject.getAttestationStatement().get("x5c");
 
         if (x5cNode != null && x5cNode.isArray()) {
@@ -25,7 +25,11 @@ public interface X5cAttestationStatementVerifier {
 
             for (JsonNode binary : x5cNode) {
                 if (binary.isBinary()) {
-                    certs.add(CertificateParser.parseDer(binary.binaryValue()));
+                    try {
+                        certs.add(CertificateParser.parseDer(binary.binaryValue()));
+                    } catch (IOException e) {
+                        throw new RuntimeException("binary.isBinary() was true but binary.binaryValue() failed", e);
+                    }
                 } else {
                     throw new IllegalArgumentException(String.format(
                         "Each element of \"x5c\" property of attestation statement must be a binary value, was: %s",
