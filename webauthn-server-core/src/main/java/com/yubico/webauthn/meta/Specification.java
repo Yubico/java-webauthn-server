ad6827e3706c0a5ef3e44acede40419b556e7eb5
Emil Lundberg <emil@yubico.com>
2018-08-20 13:03:35 +0200

    Translate Specification to Java

---
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/meta/Specification.java
@@ -0,0 +1,40 @@
+package com.yubico.webauthn.meta;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yubico.webauthn.DocumentStatus;
+import com.yubico.webauthn.impl.json.LocalDateJsonSerializer;
+import java.net.URL;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Value;
+
+
+/**
+ * Reference to a particular version of a specification document.
+ */
+@Value
+@Builder
+public class Specification {
+
+    /**
+     * Address to this version of the specification.
+     */
+    private final URL url;
+
+    /**
+     * Address to the latest version of this specification.
+     */
+    private final URL latestVersionUrl;
+
+    /**
+     * An object indicating the status of the specification document.
+     */
+    private final DocumentStatus status;
+
+    /**
+     * The release date of the specification document.
+     */
+    @JsonSerialize(using = LocalDateJsonSerializer.class)
+    private final LocalDate releaseDate;
+
+}
--- webauthn-server-core/src/main/scala/com/yubico/webauthn/version-info.scala
+++ webauthn-server-core/src/main/scala/com/yubico/webauthn/version-info.scala
@@ -4,30 +4,15 @@ import java.net.URL
 import java.time.LocalDate
 import java.util.Optional
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.yubico.webauthn.impl.json.LocalDateJsonSerializer
 import com.yubico.webauthn.meta.Implementation
+import com.yubico.webauthn.meta.Specification
 
 
 
 
 
 
-/**
-  * Reference to a particular version of a specification document.
-  *
-  * @param url Address to this version of the specification.
-  * @param latestVersionUrl Address to the latest version of this specification.
-  * @param status An object indicating the status of the specification document.
-  * @param releaseDate The release date of the specification document.
-  */
-case class Specification(
-  url: URL,
-  latestVersionUrl: URL,
-  status: DocumentStatus,
-  @JsonSerialize(using = classOf[LocalDateJsonSerializer])
-  releaseDate: LocalDate
-)
+
 
 /**
   * Contains version information for the com.yubico.webauthn package.
@@ -39,12 +24,12 @@ object VersionInfo {
   /**
     * Represents the specification this implementation is based on
     */
-  val specification = Specification(
-    url = new URL("https://www.w3.org/TR/2018/CR-webauthn-20180320/"),
-    latestVersionUrl = new URL("https://www.w3.org/TR/webauthn/"),
-    status = DocumentStatus.CANDIDATE_RECOMMENDATION,
-    releaseDate = LocalDate.parse("2018-03-20")
-  )
+  val specification = Specification.builder()
+    .url(new URL("https://www.w3.org/TR/2018/CR-webauthn-20180320/"))
+    .latestVersionUrl(new URL("https://www.w3.org/TR/webauthn/"))
+    .status(DocumentStatus.CANDIDATE_RECOMMENDATION)
+    .releaseDate(LocalDate.parse("2018-03-20"))
+    .build()
 
   /**
     * Represents the specification this implementation is based on
