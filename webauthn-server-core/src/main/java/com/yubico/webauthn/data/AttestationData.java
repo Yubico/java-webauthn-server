9983018df0716b23f31b1b0725379b3b7cffe925
Emil Lundberg <emil@yubico.com>
2018-08-21 14:44:49 +0200

    Make AttestationData constructor private

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/AttestationData.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/AttestationData.java
@@ -4,10 +4,13 @@ import COSE.CoseException;
 import com.yubico.webauthn.impl.util.WebAuthnCodecs;
 import java.io.IOException;
 import java.security.interfaces.ECPublicKey;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class AttestationData {
 
