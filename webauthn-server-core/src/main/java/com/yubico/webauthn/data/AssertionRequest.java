9ab2d1facf8f771f18d9bbffca1b1918bd6af948
Emil Lundberg <emil@yubico.com>
2018-08-21 14:42:37 +0200

    Delete unnecessary code from AssertionRequest

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/AssertionRequest.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/AssertionRequest.java
@@ -2,25 +2,17 @@ package com.yubico.webauthn.data;
 
 import java.util.Optional;
 import lombok.Builder;
-import lombok.NonNull;
 import lombok.Value;
 
 
 @Value
 @Builder
 public class AssertionRequest {
-    private ByteArray requestId;
-    private Optional<String> username;
-    private PublicKeyCredentialRequestOptions publicKeyCredentialRequestOptions;
 
-    private AssertionRequest(
-        @NonNull ByteArray requestId,
-        @NonNull Optional<String> username,
-        @NonNull PublicKeyCredentialRequestOptions publicKeyCredentialRequestOptions
-    ) {
-        this.requestId = requestId;
-        this.username = username;
-        this.publicKeyCredentialRequestOptions = publicKeyCredentialRequestOptions;
-    }
+    private final ByteArray requestId;
+    private final PublicKeyCredentialRequestOptions publicKeyCredentialRequestOptions;
+
+    @Builder.Default
+    private final Optional<String> username = Optional.empty();
 
 }
