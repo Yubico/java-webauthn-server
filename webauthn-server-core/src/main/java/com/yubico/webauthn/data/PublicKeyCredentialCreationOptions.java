ea0bc5c77f79e62f8bca9e9d7787480eaa69481b
Emil Lundberg <emil@yubico.com>
2018-08-21 16:10:07 +0200

    Clean up PublicKeyCredentialCreationOptions

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/PublicKeyCredentialCreationOptions.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/PublicKeyCredentialCreationOptions.java
@@ -9,26 +9,30 @@ import java.util.Optional;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.Value;
 
 
-@Getter
+@Value
 @Builder(toBuilder = true)
 public class PublicKeyCredentialCreationOptions {
 
     /**
      * Contains data about the Relying Party responsible for the request.
      */
-    private RelyingPartyIdentity rp;
+    @NonNull
+    private final RelyingPartyIdentity rp;
 
     /**
      * Contains data about the user account for which the Relying Party is requesting attestation.
      */
-    private UserIdentity user;
+    @NonNull
+    private final UserIdentity user;
 
     /**
      * A challenge intended to be used for generating the newly created credential’s attestation object.
      */
-    private ByteArray challenge;
+    @NonNull
+    private final ByteArray challenge;
 
     /**
      * Information about the desired properties of the credential to be created.
@@ -36,36 +40,41 @@ public class PublicKeyCredentialCreationOptions {
      * The sequence is ordered from most preferred to least preferred. The client will make a best-effort to create the
      * most preferred credential that it can.
      */
-    private List<PublicKeyCredentialParameters> pubKeyCredParams;
+    @NonNull
+    private final List<PublicKeyCredentialParameters> pubKeyCredParams;
 
     /**
      * Specifies a time, in milliseconds, that the caller is willing to wait for the call to complete. This is treated
      * as a hint, and MAY be overridden by the platform.
      */
+    @NonNull
     @Builder.Default
-    private Optional<Long> timeout = Optional.empty();
+    private final Optional<Long> timeout = Optional.empty();
 
     /**
      * Intended for use by Relying Parties that wish to limit the creation of multiple credentials for the same account
      * on a single authenticator. The client is requested to return an error if the new credential would be created on
      * an authenticator that also contains one of the credentials enumerated in this parameter.
      */
+    @NonNull
     @Builder.Default
-    private Optional<Collection<PublicKeyCredentialDescriptor>> excludeCredentials = Optional.empty();
+    private final Optional<Collection<PublicKeyCredentialDescriptor>> excludeCredentials = Optional.empty();
 
     /**
      * Intended for use by Relying Parties that wish to select the appropriate authenticators to participate in the
      * create() operation.
      */
+    @NonNull
     @Builder.Default
-    private Optional<AuthenticatorSelectionCriteria> authenticatorSelection = Optional.empty();
+    private final Optional<AuthenticatorSelectionCriteria> authenticatorSelection = Optional.empty();
 
     /**
      * Intended for use by Relying Parties that wish to express their preference for attestation conveyance. The default
      * is none.
      */
+    @NonNull
     @Builder.Default
-    private AttestationConveyancePreference attestation = AttestationConveyancePreference.DEFAULT;
+    private final AttestationConveyancePreference attestation = AttestationConveyancePreference.DEFAULT;
 
     /**
      * Additional parameters requesting additional processing by the client and authenticator.
@@ -75,8 +84,9 @@ public class PublicKeyCredentialCreationOptions {
      * §8 WebAuthn Extensions; consult the IANA "WebAuthn Extension Identifier" registry  for an up-to-date list of
      * registered WebAuthn Extensions.
      */
+    @NonNull
     @Builder.Default
-    private Optional<JsonNode> extensions = Optional.empty();
+    private final Optional<JsonNode> extensions = Optional.empty();
 
     private PublicKeyCredentialCreationOptions(
         @NonNull RelyingPartyIdentity rp,
