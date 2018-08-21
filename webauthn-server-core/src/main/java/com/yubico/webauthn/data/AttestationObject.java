2b76715a82bf15906e5234ea7f4bd8838d8a3ffd
Emil Lundberg <emil@yubico.com>
2018-08-21 14:47:17 +0200

    Clean up AttestationObject

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/AttestationObject.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/AttestationObject.java
@@ -1,31 +1,43 @@
 package com.yubico.webauthn.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.yubico.webauthn.exception.Base64UrlException;
 import com.yubico.webauthn.impl.util.WebAuthnCodecs;
 import java.io.IOException;
+import lombok.NonNull;
 import lombok.Value;
 
 
 @Value
 public class AttestationObject {
 
+    @JsonIgnore
+    @NonNull
     private final ByteArray bytes;
 
-    private ObjectNode decoded;
+    @JsonIgnore
+    @NonNull
+    private final ObjectNode decoded;
 
-    @JsonProperty
-    private AuthenticatorData authenticatorData;
+    @NonNull
+    @JsonIgnore
+    private final AuthenticatorData authenticatorData;
+
+    @NonNull
+    @JsonProperty("authData")
+    private final ByteArray authData;
 
     /**
      * The ''attestation statement format'' of this attestation object.
      */
-    @JsonProperty
+    @NonNull
+    @JsonProperty("fmt")
     private final String format;
 
-    public AttestationObject(ByteArray bytes) throws IOException, Base64UrlException {
+    public AttestationObject(@NonNull ByteArray bytes) throws IOException, Base64UrlException {
         this.bytes = bytes;
 
         JsonNode decoded = WebAuthnCodecs.cbor().readTree(bytes.getBytes());
@@ -35,21 +47,18 @@ public class AttestationObject {
             throw new IllegalArgumentException("Attestation object must be a JSON object.");
         }
 
-        authenticatorData = parseAuthenticatorData();
+        JsonNode authData = decoded.get("authData");
+        if (authData.isBinary()) {
+            this.authData = new ByteArray(authData.binaryValue());
+        } else {
+            this.authData = ByteArray.fromBase64Url(authData.textValue());
+        }
 
+        authenticatorData = new AuthenticatorData(this.authData);
         format = decoded.get("fmt").textValue();
     }
 
-    private AuthenticatorData parseAuthenticatorData() throws IOException, Base64UrlException {
-        JsonNode authData = decoded.get("authData");
-        if (authData.isBinary()) {
-            return new AuthenticatorData(new ByteArray(authData.binaryValue()));
-        } else {
-            return new AuthenticatorData(ByteArray.fromBase64Url(authData.textValue()));
-        }
-    }
-
-    @JsonProperty
+    @JsonProperty("attStmt")
     public JsonNode getAttestationStatement() {
         return decoded.get("attStmt");
     }
