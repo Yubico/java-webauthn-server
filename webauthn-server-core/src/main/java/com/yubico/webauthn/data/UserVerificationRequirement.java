70e164de5e07ecb86e4443e866973c054401e401
Emil Lundberg <emil@yubico.com>
2018-09-21 13:47:20 +0200

    Make UserVerificationRequirement JSON deserializable

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/UserVerificationRequirement.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/UserVerificationRequirement.java
@@ -1,8 +1,11 @@
 package com.yubico.webauthn.data;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.yubico.internal.util.json.JsonStringSerializable;
 import com.yubico.internal.util.json.JsonStringSerializer;
+import java.util.Optional;
+import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
@@ -18,6 +21,17 @@ public enum UserVerificationRequirement implements JsonStringSerializable {
     private final String id;
 
     public static final UserVerificationRequirement DEFAULT = PREFERRED;
+
+    private static Optional<UserVerificationRequirement> fromString(@NonNull String id) {
+        return Stream.of(values()).filter(v -> v.id.equals(id)).findAny();
+    }
+
+    @JsonCreator
+    private static UserVerificationRequirement fromJsonString(@NonNull String id) {
+        return fromString(id).orElseThrow(() -> new IllegalArgumentException(String.format(
+            "Unknown %s value: %s", UserVerificationRequirement.class.getSimpleName(), id
+        )));
+    }
 
     @Override
     public String toJsonString() {
