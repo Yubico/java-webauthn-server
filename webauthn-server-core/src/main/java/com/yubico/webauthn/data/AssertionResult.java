e47fee1d20cebb0dfd7ebd8cd39dba666ae93a28
Emil Lundberg <emil@yubico.com>
2018-09-21 14:02:44 +0200

    Make AssertionResult JSON deserializable

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/AssertionResult.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/AssertionResult.java
@@ -1,5 +1,7 @@
 package com.yubico.webauthn.data;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Collections;
 import java.util.List;
 import lombok.Builder;
@@ -29,14 +31,15 @@ public class AssertionResult {
     @NonNull
     private final List<String> warnings;
 
+    @JsonCreator
     private AssertionResult(
-        @NonNull ByteArray credentialId,
-        @NonNull ByteArray userHandle,
-        long signatureCount,
-        boolean signatureCounterValid,
-        boolean success,
-        @NonNull String username,
-        @NonNull List<String> warnings
+        @NonNull @JsonProperty("credentialId") ByteArray credentialId,
+        @NonNull @JsonProperty("userHandle") ByteArray userHandle,
+        @JsonProperty("signatureCount") long signatureCount,
+        @JsonProperty("signatureCounterValid") boolean signatureCounterValid,
+        @JsonProperty("success") boolean success,
+        @NonNull @JsonProperty("username") String username,
+        @NonNull @JsonProperty("warnings") List<String> warnings
     ) {
         this.credentialId = credentialId;
         this.userHandle = userHandle;
