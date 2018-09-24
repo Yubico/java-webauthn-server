9db41be223967d5e4f763a104020a119a1fa4076
Emil Lundberg <emil@yubico.com>
2018-09-24 15:30:16 +0200

    Rearrange CollectedClientData implementation
    - Delete field `clientData`
    - Remove `transient` from other fields
    - Parse `tokenBinding` in constructor instead of getter
    - Add @JsonCreator constructor

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/CollectedClientData.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/CollectedClientData.java
@@ -1,5 +1,7 @@
 package com.yubico.webauthn.data;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.yubico.internal.util.ExceptionUtil;
@@ -17,97 +19,134 @@ import lombok.Value;
 public class CollectedClientData {
 
     /**
-     * The client data returned from, or to be sent to, the client.
-     */
-    @NonNull
-    private ObjectNode clientData;
-
-    /**
      * The URL-safe Base64 encoded challenge as provided by the RP.
      */
     @NonNull
-    private final transient ByteArray challenge;
+    private final ByteArray challenge;
 
     /**
      * The fully qualified origin of the requester, as identified by the client.
      */
     @NonNull
-    private final transient String origin;
+    private final String origin;
 
     /**
      * The type of the requested operation, set by the client.
      */
     @NonNull
-    private final transient String type;
+    private final String type;
 
     /**
      * Input or output values for or from authenticator extensions, if any.
      */
     @NonNull
-    private final transient Optional<ObjectNode> authenticatorExtensions;
+    private final Optional<ObjectNode> authenticatorExtensions;
 
     /**
      * Input or output values for or from client extensions, if any.
      */
     @NonNull
-    private final transient Optional<ObjectNode> clientExtensions;
+    private final Optional<ObjectNode> clientExtensions;
 
-    public CollectedClientData(@NonNull ByteArray clientDataJSON) throws IOException, Base64UrlException {
-        this(WebAuthnCodecs.json().readTree(clientDataJSON.getBytes()));
+    /**
+     * The URL-safe Base64 encoded TLS token binding ID the client has negotiated with the RP.
+     */
+    @NonNull
+    private final Optional<TokenBindingInfo> tokenBinding;
+
+    public CollectedClientData(
+        /**
+         * The client data returned from, or to be sent to, the client.
+         */
+        @NonNull ByteArray clientDataJSON
+    ) throws IOException, Base64UrlException {
+        this(parseJson(clientDataJSON));
     }
 
-    private CollectedClientData(@NonNull JsonNode clientData) throws Base64UrlException {
+    @JsonCreator
+    private CollectedClientData(
+        @NonNull @JsonProperty("challenge") ByteArray challenge,
+        @NonNull @JsonProperty("origin") String origin,
+        @NonNull @JsonProperty("type") String type,
+        @NonNull @JsonProperty("authenticatorExtensions") Optional<JsonNode> authenticatorExtensions,
+        @NonNull @JsonProperty("clientExtensions") Optional<JsonNode> clientExtensions,
+        @NonNull @JsonProperty("tokenBinding") Optional<TokenBindingInfo> tokenBinding
+    ) {
+        this.challenge = challenge;
+        this.origin = origin;
+        this.type = type;
+
+        authenticatorExtensions.ifPresent(ae -> {
+            ExceptionUtil.assure(
+                ae.isObject(),
+                "Field \"authenticatorExtensions\" must be an object if present."
+            );
+        });
+        this.authenticatorExtensions = authenticatorExtensions.map(ae -> (ObjectNode) ae).map(WebAuthnCodecs::deepCopy);
+
+        clientExtensions.ifPresent(ce -> {
+            ExceptionUtil.assure(
+                ce.isObject(),
+                "Field \"clientExtensions\" must be an object if present."
+            );
+        });
+        this.clientExtensions = clientExtensions.map(ce -> (ObjectNode) ce).map(WebAuthnCodecs::deepCopy);
+
+        this.tokenBinding = tokenBinding;
+    }
+
+    private CollectedClientData(
+        @NonNull ObjectNode clientData
+    ) throws Base64UrlException {
+        this(
+            parseChallenge(clientData),
+            parseOrigin(clientData),
+            parseType(clientData),
+            Optional.ofNullable(clientData.get("authenticatorExtensions")),
+            Optional.ofNullable(clientData.get("clientExtensions")),
+            parseTokenBinding(clientData.get("tokenBinding"))
+        );
+    }
+
+    private static ObjectNode parseJson(@NonNull ByteArray jsonBytes) throws IOException {
+        final JsonNode clientData = WebAuthnCodecs.json().readTree(jsonBytes.getBytes());
         ExceptionUtil.assure(
             clientData != null && clientData.isObject(),
             "Collected client data must be JSON object."
         );
+        return (ObjectNode) clientData;
+    }
 
-        this.clientData = (ObjectNode) clientData;
-
+    private static ByteArray parseChallenge(@NonNull JsonNode clientData) throws Base64UrlException {
         try {
-            challenge = ByteArray.fromBase64Url(clientData.get("challenge").textValue());
+            return ByteArray.fromBase64Url(clientData.get("challenge").textValue());
         } catch (NullPointerException e) {
             throw new IllegalArgumentException("Missing field: \"challenge\"");
         } catch (Base64UrlException e) {
             throw new Base64UrlException("Invalid \"challenge\" value", e);
         }
+    }
 
+    private static String parseOrigin(@NonNull JsonNode clientData) {
+        String origin;
         try {
             origin = clientData.get("origin").textValue();
         } catch (NullPointerException e) {
             throw new IllegalArgumentException("Missing field: \"origin\"");
         }
+        return origin;
+    }
 
+    private static String parseType(@NonNull JsonNode clientData) {
         try {
-            type = clientData.get("type").textValue();
+            return clientData.get("type").textValue();
         } catch (NullPointerException e) {
             throw new IllegalArgumentException("Missing field: \"type\"");
         }
-
-        final JsonNode authenticatorExtensions = clientData.get("authenticatorExtensions");
-        if (authenticatorExtensions == null) {
-            this.authenticatorExtensions = Optional.empty();
-        } else if (authenticatorExtensions.isObject()) {
-            this.authenticatorExtensions = Optional.of(WebAuthnCodecs.deepCopy((ObjectNode) authenticatorExtensions));
-        } else {
-            throw new IllegalArgumentException("Field \"authenticatorExtensions\" must be an object if present.");
-        }
-
-        final JsonNode clientExtensions = clientData.get("clientExtensions");
-        if (clientExtensions == null) {
-            this.clientExtensions = Optional.empty();
-        } else if (clientExtensions.isObject()) {
-            this.clientExtensions = Optional.of(WebAuthnCodecs.deepCopy((ObjectNode) clientExtensions));
-        } else {
-            throw new IllegalArgumentException("Field \"clientExtensions\" must be an object if present.");
-        }
     }
 
-    /**
-     * The URL-safe Base64 encoded TLS token binding ID the client has negotiated with the RP.
-     */
-    public final Optional<TokenBindingInfo> getTokenBinding() {
-        return Optional.ofNullable(clientData.get("tokenBinding"))
+    public static final Optional<TokenBindingInfo> parseTokenBinding(JsonNode tokenBinding) {
+        return Optional.ofNullable(tokenBinding)
             .map(tb -> {
                 if (tb.isObject()) {
                     String status = tb.get("status").textValue();
@@ -124,7 +163,7 @@ public class CollectedClientData {
                             })
                     );
                 } else {
-                    throw new IllegalArgumentException("Property \"tokenBinding\" missing from client data.");
+                    throw new IllegalArgumentException("Property \"tokenBinding\" must be a JSON object if present, was: " + tb);
                 }
             });
     }
