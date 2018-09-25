150484f6aab25ea2c0e6efc346f8515307a06dca
Emil Lundberg <emil@yubico.com>
2018-09-25 16:23:11 +0200

    Add JacksonGenerators.arbitraryCborObject

--- webauthn-server-core/src/test/scala/com/yubico/scalacheck/gen/JacksonGenerators.scala
+++ webauthn-server-core/src/test/scala/com/yubico/scalacheck/gen/JacksonGenerators.scala
@@ -3,6 +3,7 @@ package com.yubico.scalacheck.gen
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import com.fasterxml.jackson.databind.node.ObjectNode
+import com.upokecenter.cbor.CBORObject
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary.arbitrary
 
@@ -14,5 +15,14 @@ object JacksonGenerators {
   private def jsonFactory: JsonNodeFactory = JsonNodeFactory.instance
   implicit val arbitraryJsonNode: Arbitrary[JsonNode] = Arbitrary(arbitrary[String] map (value => jsonFactory.textNode(value)))
   implicit val arbitraryObjectNode: Arbitrary[ObjectNode] = Arbitrary(arbitrary[Map[String, _ <: JsonNode]] map (exts => { val o = jsonFactory.objectNode(); o.setAll(exts.asJava); o }))
+
+  implicit val arbitraryCborObject: Arbitrary[CBORObject] = Arbitrary(for {
+    key <- arbitrary[String]
+    value <- arbitrary[String]
+  } yield {
+    val o = CBORObject.NewMap()
+    o.set(key, CBORObject.FromObject(value))
+    o
+  })
 
 }
