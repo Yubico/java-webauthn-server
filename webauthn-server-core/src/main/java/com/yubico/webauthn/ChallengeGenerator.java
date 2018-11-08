e6fcbe44d12947084b76890818bae954b901be8e
Emil Lundberg <emil@yubico.com>
2018-11-08 18:05:52 +0100

    Fix some copyright headers

--- webauthn-server-core/src/main/java/com/yubico/webauthn/ChallengeGenerator.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/ChallengeGenerator.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2014 Yubico.
+ * Copyright 2014-2018 Yubico.
  * Copyright 2014 Google Inc. All rights reserved.
  *
  * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file or at
+ * license that can be found in the COPYING file or at
  * https://developers.google.com/open-source/licenses/bsd
  */
 
--- yubico-util/src/main/java/com/yubico/internal/util/ByteInputStream.java
+++ yubico-util/src/main/java/com/yubico/internal/util/ByteInputStream.java
@@ -1,8 +1,8 @@
 /*
- * Copyright 2014 Yubico.
+ * Copyright 2014-2018 Yubico.
  *
  * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
+ * license that can be found in the COPYING file.
  */
 
 package com.yubico.internal.util;
