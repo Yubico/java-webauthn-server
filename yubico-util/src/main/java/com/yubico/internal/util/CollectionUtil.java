69c2ba37b44caa7701140dbf5fdcb52d1522b3bc
Emil Lundberg <emil@yubico.com>
2018-12-12 20:47:07 +0100

    Add function CollectionUtil.immmutableSet

--- yubico-util/src/main/java/com/yubico/internal/util/CollectionUtil.java
+++ yubico-util/src/main/java/com/yubico/internal/util/CollectionUtil.java
@@ -2,12 +2,18 @@ package com.yubico.internal.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionUtil {
 
     public static <T> List<T> immutableList(List<T> l) {
         return Collections.unmodifiableList(new ArrayList<>(l));
+    }
+
+    public static <T> Set<T> immutableSet(Set<T> s) {
+        return Collections.unmodifiableSet(new HashSet<>(s));
     }
 
 }
--- yubico-util/src/test/scala/com/yubico/internal/util/CollectionUtilSpec.scala
+++ yubico-util/src/test/scala/com/yubico/internal/util/CollectionUtilSpec.scala
@@ -11,7 +11,6 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 class CollectionUtilSpec extends FunSpec with Matchers with GeneratorDrivenPropertyChecks {
 
   describe("immutableList") {
-
     it("returns a List instance which throws exceptions on modification attempts.") {
       forAll { l: java.util.List[Int] =>
         val immutable = CollectionUtil.immutableList(l)
@@ -34,7 +33,30 @@ class CollectionUtilSpec extends FunSpec with Matchers with GeneratorDrivenPrope
         immutable.size should equal (l.size - 1)
       }
     }
-
   }
 
+  describe("immutableSet") {
+    it("returns a Set instance which throws exceptions on modification attempts.") {
+      forAll { s: java.util.Set[Int] =>
+        val immutable = CollectionUtil.immutableSet(s)
+        an [UnsupportedOperationException] should be thrownBy { immutable.add(0) }
+      }
+
+      forAll(minSize(1)) { s: java.util.Set[Int] =>
+        val immutable = CollectionUtil.immutableSet(s)
+        an [UnsupportedOperationException] should be thrownBy { immutable.remove(0) }
+      }
+    }
+
+    it("prevents mutations to the argument from propagating to the return value.") {
+      forAll { s: java.util.Set[Int] =>
+        val immutable = CollectionUtil.immutableSet(s)
+        immutable should equal (s)
+
+        s.add(0.to(10000).find(i => !s.contains(i)).get)
+        immutable should not equal s
+        immutable.size should equal (s.size - 1)
+      }
+    }
+  }
 }
