7b9d521425ed151dc18ef0374ea461fab7f40a17
Emil Lundberg <emil@yubico.com>
2018-12-12 20:50:07 +0100

    Add javadoc to CollectionUtil functions

--- yubico-util/src/main/java/com/yubico/internal/util/CollectionUtil.java
+++ yubico-util/src/main/java/com/yubico/internal/util/CollectionUtil.java
@@ -8,10 +8,16 @@ import java.util.Set;
 
 public class CollectionUtil {
 
+    /**
+     * @return A shallow copy of <code>l</code> which cannot be modified
+     */
     public static <T> List<T> immutableList(List<T> l) {
         return Collections.unmodifiableList(new ArrayList<>(l));
     }
 
+    /**
+     * @return A shallow copy of <code>s</code> which cannot be modified
+     */
     public static <T> Set<T> immutableSet(Set<T> s) {
         return Collections.unmodifiableSet(new HashSet<>(s));
     }
