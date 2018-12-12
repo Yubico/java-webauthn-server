ce55e1df3e0a7be4460ae07f8a6cddd917bdec77
Emil Lundberg <emil@yubico.com>
2018-12-12 20:39:26 +0100

    Make JavaGenerators generate mutable collections

--- yubico-util-scala/src/main/scala/com/yubico/scalacheck/gen/JavaGenerators.scala
+++ yubico-util-scala/src/main/scala/com/yubico/scalacheck/gen/JavaGenerators.scala
@@ -17,13 +17,13 @@ object JavaGenerators {
     Arbitrary(Gen.option(a.arbitrary).map(_.asJava))
 
   implicit def arbitraryList[A](implicit a: Arbitrary[List[A]]): Arbitrary[java.util.List[A]] =
-    Arbitrary(a.arbitrary map (_.asJava))
+    Arbitrary(a.arbitrary map (l => new java.util.ArrayList[A](l.asJava)))
 
   implicit def arbitraryMap[A, B](implicit a: Arbitrary[Map[A, B]]): Arbitrary[java.util.Map[A, B]] =
-    Arbitrary(a.arbitrary map (_.asJava))
+    Arbitrary(a.arbitrary map (m => new java.util.HashMap[A, B](m.asJava)))
 
   implicit def arbitrarySet[A](implicit a: Arbitrary[Set[A]]): Arbitrary[java.util.Set[A]] =
-    Arbitrary(a.arbitrary map (_.asJava))
+    Arbitrary(a.arbitrary map (s => new java.util.HashSet[A](s.asJava)))
 
   implicit val arbitraryUrl: Arbitrary[URL] = Arbitrary(url())
   def url(
