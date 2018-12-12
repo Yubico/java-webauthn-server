f88fcdb8165efde2bd2b22bc08cd4f269660fda4
Emil Lundberg <emil@yubico.com>
2018-12-12 20:34:02 +0100

    Move JavaGenerators to project yubico-util-scala

---
+++ yubico-util-scala/src/main/scala/com/yubico/scalacheck/gen/JavaGenerators.scala
@@ -0,0 +1,36 @@
+package com.yubico.scalacheck.gen
+
+import java.net.URL
+import java.util.Optional
+
+import com.yubico.internal.util.scala.JavaConverters._
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Arbitrary.arbitrary
+
+import scala.collection.JavaConverters._
+
+
+object JavaGenerators {
+
+  implicit def arbitraryOptional[A](implicit a: Arbitrary[A]): Arbitrary[Optional[A]] = Arbitrary(Gen.option(a.arbitrary).map(_.asJava))
+
+  implicit def arbitraryList[A](implicit a: Arbitrary[List[A]]): Arbitrary[java.util.List[A]] = org.scalacheck.Arbitrary(a.arbitrary map (_.asJava))
+  implicit def arbitraryMap[A, B](implicit a: Arbitrary[Map[A, B]]): Arbitrary[java.util.Map[A, B]] = org.scalacheck.Arbitrary(a.arbitrary map (_.asJava))
+  implicit def arbitrarySet[A](implicit a: Arbitrary[Set[A]]): Arbitrary[java.util.Set[A]] = org.scalacheck.Arbitrary(a.arbitrary map (_.asJava))
+
+  implicit val arbitraryUrl: Arbitrary[URL] = Arbitrary(url())
+  def url(
+    scheme: Gen[String] = Gen.oneOf("http", "https"),
+    host: Gen[String] = Gen.alphaNumStr,
+    path: Gen[String] = Gen.alphaNumStr
+  ): Gen[URL] = for {
+    scheme <- scheme
+    host <- host
+    path <- path
+  } yield new URL(s"${scheme}://${host}${if (path.isEmpty) "" else "/"}${path}")
+
+  implicit val arbitraryBoolean: Arbitrary[java.lang.Boolean] = Arbitrary(arbitrary[Boolean].map((a: Boolean) => a))
+  implicit val arbitraryLong: Arbitrary[java.lang.Long] = Arbitrary(arbitrary[Long].map((a: Long) => a))
+
+}
