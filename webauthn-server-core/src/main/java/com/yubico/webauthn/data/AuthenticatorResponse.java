0382594b780122f5ff470ec71504218d6c8f1a0a
Emil Lundberg <emil@yubico.com>
2018-12-19 18:10:07 +0100

    Delete unnecessary JSON annotations

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/AuthenticatorResponse.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/AuthenticatorResponse.java
@@ -25,19 +25,16 @@
 package com.yubico.webauthn.data;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 public interface AuthenticatorResponse {
 
     ByteArray getAuthenticatorData();
 
     @JsonIgnore
-    @JsonProperty("_authenticatorData")
     default AuthenticatorData getParsedAuthenticatorData() {
         return new AuthenticatorData(getAuthenticatorData());
     }
 
-    @JsonProperty("clientDataJSON")
     ByteArray getClientDataJSON();
 
     /**
