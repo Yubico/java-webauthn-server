7ebb878da75b981b39466648986745e91dc184c9
Emil Lundberg <emil@yubico.com>
2018-12-21 16:23:30 +0100

    Update javadoc of PublicKeyCredentialRequestOptions

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/PublicKeyCredentialRequestOptions.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/PublicKeyCredentialRequestOptions.java
@@ -38,6 +38,11 @@ import lombok.Value;
  * The PublicKeyCredentialRequestOptions dictionary supplies get() with the data it needs to generate an assertion.
  * <p>
  * Its `challenge` member must be present, while its other members are optional.
+ * </p>
+ *
+ * @see <a href="https://w3c.github.io/webauthn/#dictdef-publickeycredentialrequestoptions">§5.5. Options for Assertion
+ * Generation (dictionary PublicKeyCredentialRequestOptions)
+ * </a>
  */
 @Value
 @Builder(toBuilder = true)
@@ -45,7 +50,8 @@ public class PublicKeyCredentialRequestOptions {
 
     /**
      * A challenge that the selected authenticator signs, along with other data, when producing an authentication
-     * assertion.
+     * assertion. See the <a href="https://w3c.github.io/webauthn/#cryptographic-challenges">§13.1 Cryptographic
+     * Challenges</a> security consideration.
      */
     @NonNull
     private final ByteArray challenge;
@@ -53,7 +59,8 @@ public class PublicKeyCredentialRequestOptions {
     /**
      * Specifies a time, in milliseconds, that the caller is willing to wait for the call to complete.
      * <p>
-     * This is treated as a hint, and MAY be overridden by the platform.
+     * This is treated as a hint, and MAY be overridden by the client.
+     * </p>
      */
     @NonNull
     @Builder.Default
@@ -63,22 +70,27 @@ public class PublicKeyCredentialRequestOptions {
      * Specifies the relying party identifier claimed by the caller.
      * <p>
      * If omitted, its value will be set by the client.
+     * </p>
      */
     @NonNull
     @Builder.Default
     private final Optional<String> rpId = Optional.empty();
 
     /**
-     * A list of public key credentials acceptable to the caller, in descending order of the caller’s preference.
+     * A list of {@link PublicKeyCredentialDescriptor} objects representing public key credentials acceptable to the
+     * caller, in descending order of the caller’s preference (the first item in the list is the most preferred
+     * credential, and so on down the list).
      */
     @NonNull
     @Builder.Default
     private final Optional<List<PublicKeyCredentialDescriptor>> allowCredentials = Optional.empty();
 
     /**
-     * Describes the Relying Party's requirements regarding user verification for the get() operation.
+     * Describes the Relying Party's requirements regarding <a href="https://w3c.github.io/webauthn/#user-verification">user
+     * verification</a> for the <code>navigator.credentials.get()</code> operation.
      * <p>
      * Eligible authenticators are filtered to only those capable of satisfying this requirement.
+     * </p>
      */
     @NonNull
     @Builder.Default
@@ -89,6 +101,7 @@ public class PublicKeyCredentialRequestOptions {
      * <p>
      * For example, if transaction confirmation is sought from the user, then the prompt string might be included as an
      * extension.
+     * </p>
      */
     @NonNull
     @Builder.Default
