eb335413939e9d334afe97271b4d5c39de443000
Emil Lundberg <emil@yubico.com>
2019-01-08 20:23:21 +0100

    Fix <p> tags in AuthenticatorTransport Javadoc

--- webauthn-server-core/src/main/java/com/yubico/webauthn/data/AuthenticatorTransport.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/data/AuthenticatorTransport.java
@@ -41,13 +41,12 @@ import lombok.NonNull;
  * Party may obtain a list of transports hints from some attestation statement formats or via some out-of-band
  * mechanism; it is outside the scope of this specification to define that mechanism.
  * <p>
- * <p>
- * <p>
  * Authenticators may implement various transports for communicating with clients. This enumeration defines hints as to
  * how clients might communicate with a particular authenticator in order to obtain an assertion for a specific
  * credential. Note that these hints represent the WebAuthn Relying Party's best belief as to how an authenticator may
  * be reached. A Relying Party may obtain a list of transports hints from some attestation statement formats or via some
  * out-of-band mechanism; it is outside the scope of the Web Authentication specification to define that mechanism.
+ * </p>
  *
  * @see <a href="https://w3c.github.io/webauthn/#enumdef-authenticatortransport">§5.10.4. Authenticator Transport
  * Enumeration (enum AuthenticatorTransport)</a>
