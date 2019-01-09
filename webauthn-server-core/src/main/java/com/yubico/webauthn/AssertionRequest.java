29ead00b7df76dd64625ac70e0046ed9b2014897
Emil Lundberg <emil@yubico.com>
2019-01-09 15:30:40 +0100

    Add Javadoc to AssertionRequest

--- webauthn-server-core/src/main/java/com/yubico/webauthn/AssertionRequest.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/AssertionRequest.java
@@ -35,14 +35,29 @@ import lombok.NonNull;
 import lombok.Value;
 
 
+/**
+ * A combination of a {@link PublicKeyCredentialRequestOptions} and, optionally, a {@link #getUsername() username}.
+ */
 @Value
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(toBuilder = true)
 public class AssertionRequest {
 
+    /**
+     * An object that can be serialized to JSON and passed as the <code>publicKey</code> argument to
+     * <code>navigator.credentials.get()</code>.
+     */
     @NonNull
     private final PublicKeyCredentialRequestOptions publicKeyCredentialRequestOptions;
 
+    /**
+     * The username of the user to authenticate, if the user has already been identified.
+     * <p>
+     * If this is absent, this indicates that this is a request for an assertion by a <a
+     * href="https://w3c.github.io/webauthn/#client-side-resident-public-key-credential-source">client-side-resident
+     * credential</a>, and identification of the user has been deferred until the response is received.
+     * </p>
+     */
     @NonNull
     @Builder.Default
     private final Optional<String> username = Optional.empty();
@@ -63,6 +78,10 @@ public class AssertionRequest {
         public static class MandatoryStages {
             private final AssertionRequestBuilder builder = new AssertionRequestBuilder();
 
+            /**
+             * {@link AssertionRequestBuilder#publicKeyCredentialRequestOptions(PublicKeyCredentialRequestOptions)
+             * publicKeyCredentialRequestOptions} is a required parameter.
+             */
             public AssertionRequestBuilder publicKeyCredentialRequestOptions(PublicKeyCredentialRequestOptions publicKeyCredentialRequestOptions) {
                 return builder.publicKeyCredentialRequestOptions(publicKeyCredentialRequestOptions);
             }
