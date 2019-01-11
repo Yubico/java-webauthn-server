e65ffed5de790386120a2f5f951d8f5ea77c20f9
Emil Lundberg <emil@yubico.com>
2019-01-11 19:37:27 +0100

    Update VersionInfo

--- webauthn-server-core/src/main/java/com/yubico/webauthn/meta/VersionInfo.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/meta/VersionInfo.java
@@ -64,10 +64,10 @@ public class VersionInfo {
      * Represents the specification this implementation is based on
      */
     private final Specification specification = Specification.builder()
-        .url(new URL("https://www.w3.org/TR/2018/CR-webauthn-20180320/"))
-        .latestVersionUrl(new URL("https://www.w3.org/TR/webauthn/"))
-        .status(DocumentStatus.CANDIDATE_RECOMMENDATION)
-        .releaseDate(LocalDate.parse("2018-03-20"))
+        .url(new URL("https://w3c.github.io/webauthn/"))
+        .latestVersionUrl(new URL("https://w3c.github.io/webauthn/"))
+        .status(DocumentStatus.EDITORS_DRAFT)
+        .releaseDate(LocalDate.parse("2019-01-11"))
         .build();
 
     /**
