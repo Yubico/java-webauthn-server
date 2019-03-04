f6d168c22635119af062582626f7d4022c782086
Emil Lundberg <emil@yubico.com>
2019-03-04 17:23:54 +0100

    Add overload FinishRegistrationOptions.callerTokenBindingId(ByteArray)

---
+++ webauthn-server-core/src/test/java/com/yubico/webauthn/FinishRegistrationOptionsTest.java
@@ -0,0 +1,26 @@
+package com.yubico.webauthn;
+
+import com.yubico.webauthn.data.ByteArray;
+import com.yubico.webauthn.data.exception.HexException;
+import java.util.Optional;
+import org.junit.Test;
+
+public class FinishRegistrationOptionsTest {
+
+    @Test(expected = NullPointerException.class)
+    public void itHasANonOptionalCallerTokenBindingIdMethod() throws HexException {
+        FinishRegistrationOptions.builder()
+            .request(null)
+            .response(null)
+            .callerTokenBindingId(ByteArray.fromHex("aa"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void itHasAnOptionalCallerTokenBindingIdMethod() throws HexException {
+        FinishAssertionOptions.builder()
+            .request(null)
+            .response(null)
+            .callerTokenBindingId(Optional.of(ByteArray.fromHex("aa")));
+    }
+
+}
