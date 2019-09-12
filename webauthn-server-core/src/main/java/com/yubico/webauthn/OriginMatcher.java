e1ad12da52ed2bf7f0028afd11e59a57f33b8456
Emil Lundberg <emil@yubico.com>
2019-09-12 17:03:06 +0200

    Implement allowOriginPort and allowOriginSubdomain options

--- webauthn-server-core/src/main/java/com/yubico/webauthn/OriginMatcher.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/OriginMatcher.java
@@ -1,5 +1,7 @@
 package com.yubico.webauthn;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Set;
 import lombok.experimental.UtilityClass;
 
@@ -12,9 +14,38 @@ class OriginMatcher {
         boolean allowPort,
         boolean allowSubdomain
     ) {
-        return allowedOrigins.stream().anyMatch(allowedOriginString ->
-            allowedOriginString.equals(origin)
-        );
+        return allowedOrigins.stream().anyMatch(allowedOriginString -> {
+            if (allowedOriginString.equals(origin)) {
+                return true;
+            } else {
+                final URL allowedOrigin;
+                try {
+                    allowedOrigin = new URL(allowedOriginString);
+                } catch (MalformedURLException e) {
+                    return false;
+                }
+
+                final URL originUrl;
+                try {
+                    originUrl = new URL(origin);
+                } catch (MalformedURLException e) {
+                    return false;
+                }
+
+                if (!allowPort && originUrl.getPort() != allowedOrigin.getPort()) {
+                    return false;
+                }
+
+                final String allowedDomain = allowedOrigin.getHost();
+                final String originDomain = originUrl.getHost();
+
+                if (allowSubdomain) {
+                    return originDomain.endsWith("." + allowedDomain);
+                } else {
+                    return originDomain.equals(allowedDomain);
+                }
+            }
+        });
     }
 
 }
