2ff77dc8719bf0dc364505319e1bd685c9d578c6
Emil Lundberg <emil@yubico.com>
2019-10-02 14:44:06 +0200

    Simplify layout of originUrl/tmpOriginUrl code

--- webauthn-server-core/src/main/java/com/yubico/webauthn/OriginMatcher.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/OriginMatcher.java
@@ -18,17 +18,14 @@ class OriginMatcher {
     ) {
         log.trace("isAllowed({}, {}, {}, {})", origin, allowedOrigins, allowPort, allowSubdomain);
 
-        final URL originUrl;
-        {
-            URL tmpOriginUrl;
-            try {
-                tmpOriginUrl = new URL(origin);
-            } catch (MalformedURLException e) {
-                log.debug("Origin in client data is an invalid URL; will only match exactly: {}", origin);
-                tmpOriginUrl = null;
-            }
-            originUrl = tmpOriginUrl;
+        URL tmpOriginUrl;
+        try {
+            tmpOriginUrl = new URL(origin);
+        } catch (MalformedURLException e) {
+            log.debug("Origin in client data is an invalid URL; will only match exactly: {}", origin);
+            tmpOriginUrl = null;
         }
+        final URL originUrl = tmpOriginUrl;
 
         return allowedOrigins.stream().anyMatch(allowedOriginString -> {
             if (allowedOriginString.equals(origin)) {
