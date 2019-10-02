a3226603baff63a2d92cd8b5507b5804f15dbbf4
Emil Lundberg <emil@yubico.com>
2019-10-02 13:59:05 +0200

    Test that RelyingParty.origins is immutable

--- webauthn-server-core/src/test/java/com/yubico/webauthn/RelyingPartyTest.java
+++ webauthn-server-core/src/test/java/com/yubico/webauthn/RelyingPartyTest.java
@@ -3,14 +3,22 @@ package com.yubico.webauthn;
 import com.yubico.webauthn.attestation.Attestation;
 import com.yubico.webauthn.attestation.MetadataService;
 import com.yubico.webauthn.data.AttestationConveyancePreference;
+import com.yubico.webauthn.data.ByteArray;
+import com.yubico.webauthn.data.PublicKeyCredentialDescriptor;
+import com.yubico.webauthn.data.RelyingPartyIdentity;
 import com.yubico.webauthn.extension.appid.AppId;
 import com.yubico.webauthn.extension.appid.InvalidAppIdException;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class RelyingPartyTest {
 
@@ -36,6 +44,36 @@ public class RelyingPartyTest {
             .allowUntrustedAttestation(true)
             .validateSignatureCounter(true)
         ;
+    }
+
+    @Test
+    public void originsIsImmutable() {
+        Set<String> origins = new HashSet<>();
+
+        RelyingParty rp = RelyingParty.builder()
+            .identity(RelyingPartyIdentity.builder().id("localhost").name("Test").build())
+            .credentialRepository(new CredentialRepository() {
+                @Override public Set<PublicKeyCredentialDescriptor> getCredentialIdsForUsername(String username) { return null; }
+                @Override public Optional<ByteArray> getUserHandleForUsername(String username) { return Optional.empty(); }
+                @Override public Optional<String> getUsernameForUserHandle(ByteArray userHandle) { return Optional.empty(); }
+                @Override public Optional<RegisteredCredential> lookup(ByteArray credentialId, ByteArray userHandle) { return Optional.empty(); }
+                @Override public Set<RegisteredCredential> lookupAll(ByteArray credentialId) { return null; }
+            })
+            .origins(origins)
+            .build()
+        ;
+
+        assertEquals(0, rp.getOrigins().size());
+
+        origins.add("test");
+        assertEquals(0, rp.getOrigins().size());
+
+        try {
+            rp.getOrigins().add("test");
+            fail("Expected UnsupportedOperationException to be thrown");
+        } catch (UnsupportedOperationException e) {
+            assertEquals(0, rp.getOrigins().size());
+        }
     }
 
 }
