2370f12e21829be1450664ce2c0600f2875a858e
Emil Lundberg <emil@yubico.com>
2021-03-10 19:56:09 +0100

    Test that the right exception is thrown when BouncyCastle is needed but fails to load

--- test-dependent-projects/java-dep-webauthn-server-core-minimal/src/test/java/com/yubico/webauthn/BouncyCastleProviderPresenceTest.java
+++ test-dependent-projects/java-dep-webauthn-server-core-minimal/src/test/java/com/yubico/webauthn/BouncyCastleProviderPresenceTest.java
@@ -1,6 +1,12 @@
 package com.yubico.webauthn;
 
+import COSE.CoseException;
+import com.yubico.webauthn.data.AttestationObject;
 import com.yubico.webauthn.data.RelyingPartyIdentity;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -8,6 +14,7 @@ import java.security.Security;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Test that the BouncyCastle provider is not loaded by default
@@ -37,6 +44,28 @@ public class BouncyCastleProviderPresenceTest {
             .identity(RelyingPartyIdentity.builder().id("foo").name("foo").build())
             .credentialRepository(Mockito.mock(CredentialRepository.class))
             .build();
+
+        assertTrue(
+            Arrays.stream(Security.getProviders())
+                .noneMatch(prov ->
+                    prov.getName().equals("BC")
+                        || prov.getClass().getCanonicalName().contains("bouncy")
+        ));
+    }
+
+    @Test
+    public void bouncyCastleProviderIsNotLoadedAfterAttemptingToLoadEddsaKey() throws IOException, CoseException, NoSuchAlgorithmException, InvalidKeySpecException {
+        try {
+            WebAuthnCodecs.importCosePublicKey(
+                new AttestationObject(RegistrationTestData.Packed$.MODULE$.BasicAttestationEdDsa().attestationObject())
+                    .getAuthenticatorData()
+                    .getAttestedCredentialData()
+                    .get()
+                    .getCredentialPublicKey()
+            );
+        } catch (NoSuchAlgorithmException e) {
+            // OK
+        }
 
         assertTrue(
             Arrays.stream(Security.getProviders())
