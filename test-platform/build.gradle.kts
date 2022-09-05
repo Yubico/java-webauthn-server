87529b1705d38d6da327cff6a20f32b51632de57
Emil Lundberg <emil@yubico.com>
2022-09-05 16:36:13 +0200

    Move httpclient dependency version constraint from tests to main

--- test-platform/build.gradle.kts
+++ test-platform/build.gradle.kts
@@ -7,7 +7,6 @@ description = "Dependency constraints for tests"
 dependencies {
   constraints {
     api("junit:junit:[4.12,5)")
-    api("org.apache.httpcomponents:httpclient:[4.5.2,5)")
     api("org.mockito:mockito-core:[2.27.0,3)")
     api("org.scalacheck:scalacheck_2.13:[1.14.0,2)")
     api("org.scalatest:scalatest_2.13:[3.0.8,3.1)")
