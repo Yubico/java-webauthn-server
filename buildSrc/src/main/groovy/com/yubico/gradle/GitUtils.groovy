8e39b322abf4239a70e8c1d33a34d5afcfdecc3d
Emil Lundberg <emil@yubico.com>
2022-09-06 16:15:46 +0200

    Port webauthn-server-attestation/build.gradle to build.gradle.kts

---
+++ buildSrc/src/main/groovy/com/yubico/gradle/GitUtils.groovy
@@ -0,0 +1,18 @@
+package com.yubico.gradle;
+
+public class GitUtils {
+
+    public static String getGitCommit(File projectDir) {
+        def proc = "git rev-parse HEAD".execute(null, projectDir)
+        proc.waitFor()
+        if (proc.exitValue() != 0) {
+            return null
+        }
+        return proc.text.trim()
+    }
+
+    public static String getGitCommitOrUnknown(projectDir) {
+        return getGitCommit(projectDir) ?: 'UNKNOWN'
+    }
+
+}
---
+++ webauthn-server-attestation/build.gradle.kts
@@ -0,0 +1,104 @@
+import com.yubico.gradle.GitUtils;
+plugins {
+  `java-library`
+  scala
+  `maven-publish`
+  signing
+  id("com.diffplug.spotless")
+  id("info.solidsoft.pitest")
+  id("io.github.cosmicsilence.scalafix")
+}
+
+description = "Yubico WebAuthn attestation subsystem"
+
+val publishMe by extra(true)
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+sourceSets {
+  create("integrationTest") {
+    compileClasspath += sourceSets.main.get().output
+    runtimeClasspath += sourceSets.main.get().output
+  }
+}
+
+configurations["integrationTestImplementation"].extendsFrom(configurations.testImplementation.get())
+configurations["integrationTestRuntimeOnly"].extendsFrom(configurations.testRuntimeOnly.get())
+
+// Can't use test fixtures because they interfere with pitest: https://github.com/gradle/gradle/issues/12168
+evaluationDependsOn(":webauthn-server-core")
+val coreTestsOutput = project(":webauthn-server-core").extensions.getByType(SourceSetContainer::class).test.get().output
+
+dependencies {
+  api(platform(rootProject))
+
+  api(project(":webauthn-server-core"))
+
+  implementation(project(":yubico-util"))
+  implementation("com.google.guava:guava")
+  implementation("com.fasterxml.jackson.core:jackson-databind")
+  implementation("org.bouncycastle:bcprov-jdk15on")
+  implementation("org.slf4j:slf4j-api")
+
+  testImplementation(platform(project(":test-platform")))
+  testImplementation(coreTestsOutput)
+  testImplementation(project(":yubico-util-scala"))
+  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
+  testImplementation("junit:junit")
+  testImplementation("org.bouncycastle:bcpkix-jdk15on")
+  testImplementation("org.eclipse.jetty:jetty-server:[9.4.9.v20180320,10)")
+  testImplementation("org.mockito:mockito-core")
+  testImplementation("org.scala-lang:scala-library")
+  testImplementation("org.scalacheck:scalacheck_2.13")
+  testImplementation("org.scalatest:scalatest_2.13")
+  testImplementation("uk.org.lidalia:slf4j-test")
+
+  testImplementation("org.slf4j:slf4j-api") {
+    version {
+      strictly("[1.7.25,1.8-a)") // Pre-1.8 version required by slf4j-test
+    }
+  }
+}
+
+val integrationTest = task<Test>("integrationTest") {
+  description = "Runs integration tests."
+  group = "verification"
+
+  testClassesDirs = sourceSets["integrationTest"].output.classesDirs
+  classpath = sourceSets["integrationTest"].runtimeClasspath
+  shouldRunAfter(tasks.test)
+
+  // Required for processing CRL distribution points extension
+  systemProperty("com.sun.security.enableCRLDP", "true")
+}
+tasks["check"].dependsOn(integrationTest)
+
+tasks.jar {
+  manifest {
+    attributes(mapOf(
+      "Implementation-Id" to "java-webauthn-server-attestation",
+      "Implementation-Title" to project.description,
+      "Implementation-Version" to project.version,
+      "Implementation-Vendor" to "Yubico",
+      "Git-Commit" to GitUtils.getGitCommitOrUnknown(projectDir),
+    ))
+  }
+}
+
+pitest {
+  pitestVersion.set("1.4.11")
+  timestampedReports.set(false)
+
+  outputFormats.set(listOf("XML", "HTML"))
+
+  avoidCallsTo.set(listOf(
+    "java.util.logging",
+    "org.apache.log4j",
+    "org.slf4j",
+    "org.apache.commons.logging",
+    "com.google.common.io.Closeables",
+  ))
+}
