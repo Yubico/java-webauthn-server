c44ce29bbde6d64a77401a43005818588ff4fef0
Emil Lundberg <emil@yubico.com>
2022-09-06 18:13:46 +0200

    Bump and pin dependency versions for tests

--- test-platform/build.gradle.kts
+++ test-platform/build.gradle.kts
@@ -6,10 +6,10 @@ description = "Dependency constraints for tests"
 
 dependencies {
   constraints {
-    api("junit:junit:[4.12,5)")
-    api("org.mockito:mockito-core:[2.27.0,3)")
-    api("org.scalacheck:scalacheck_2.13:[1.14.0,2)")
-    api("org.scalatest:scalatest_2.13:[3.0.8,3.1)")
-    api("uk.org.lidalia:slf4j-test:[1.1.0,2)")
+    api("junit:junit:4.13.2")
+    api("org.mockito:mockito-core:4.7.0")
+    api("org.scalacheck:scalacheck_2.13:1.16.0")
+    api("org.scalatest:scalatest_2.13:3.0.9")
+    api("uk.org.lidalia:slf4j-test:1.2.0")
   }
 }
