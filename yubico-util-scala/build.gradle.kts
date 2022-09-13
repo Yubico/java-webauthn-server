cde7099fe9d645fed45505b284c4368bce7efee8
Emil Lundberg <emil@yubico.com>
2022-09-13 19:50:30 +0200

    Move TestWithEachProvider to yubico-util-scala module

--- yubico-util-scala/build.gradle.kts
+++ yubico-util-scala/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
   implementation(platform(rootProject))
   implementation(platform(project(":test-platform")))
 
+  implementation("org.bouncycastle:bcprov-jdk18on")
   implementation("org.scala-lang:scala-library")
   implementation("org.scalacheck:scalacheck_2.13")
-
-  testImplementation( "org.scalatest:scalatest_2.13")
+  implementation("org.scalatest:scalatest_2.13")
 }
