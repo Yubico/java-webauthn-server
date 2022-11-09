Yubico/java-webauthn-server

commit dc91acc80b4b822c6b0a00ba50313f358c664a09

Update test outlines and core data models to Level 2

webauthn-server-core/src/main/java/com/yubico/webauthn/RegistrationResult.java
@@ -29,12 +29,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yubico.internal.util.CollectionUtil;
 import com.yubico.webauthn.attestation.Attestation;
 import com.yubico.webauthn.data.AttestationType;
+import com.yubico.webauthn.data.AuthenticatorTransport;
 import com.yubico.webauthn.data.ByteArray;
 import com.yubico.webauthn.data.PublicKeyCredential;
 import com.yubico.webauthn.data.PublicKeyCredentialDescriptor;
+import com.yubico.webauthn.data.PublicKeyCredentialRequestOptions;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
@@ -83,6 +87,16 @@ public class RegistrationResult {
    */
   @NonNull private final ByteArray publicKeyCose;
 
+  /**
+   * The signature count returned with the created credential.
+   *
+   * <p>This is used in {@link RelyingParty#finishAssertion(FinishAssertionOptions)} to verify the
+   * validity of future signature counter values.
+   *
+   * @see RegisteredCredential#getSignatureCount() ()
+   */
+  private final long signatureCount;
+
   /** Zero or more human-readable messages about non-critical issues. */
   @NonNull @Builder.Default private final List<String> warnings = Collections.emptyList();
 
@@ -100,18 +114,36 @@ public class RegistrationResult {
    */
   private final Attestation attestationMetadata;
 
+  /**
+   * The transports returned with the created credential.
+   *
+   * <p>This is used in {@link RelyingParty#startAssertion(StartAssertionOptions)} to set the {@link
+   * PublicKeyCredentialDescriptor#getTransports() transports} members of {@link
+   * PublicKeyCredentialRequestOptions#getAllowCredentials() allowCredentials}.
+   *
+   * @see PublicKeyCredentialDescriptor#getTransports()
+   */
+  @NonNull private final SortedSet<AuthenticatorTransport> transports;
+
   @JsonCreator
   private RegistrationResult(
       @NonNull @JsonProperty("keyId") PublicKeyCredentialDescriptor keyId,
       @JsonProperty("attestationTrusted") boolean attestationTrusted,
       @NonNull @JsonProperty("attestationType") AttestationType attestationType,
       @NonNull @JsonProperty("publicKeyCose") ByteArray publicKeyCose,
+      @JsonProperty("signatureCount") Long signatureCount,
       @NonNull @JsonProperty("warnings") List<String> warnings,
-      @JsonProperty("attestationMetadata") Attestation attestationMetadata) {
+      @JsonProperty("attestationMetadata") Attestation attestationMetadata,
+      @JsonProperty("transports") Set<AuthenticatorTransport> transports) {
     this.keyId = keyId;
     this.attestationTrusted = attestationTrusted;
     this.attestationType = attestationType;
     this.publicKeyCose = publicKeyCose;
+    this.signatureCount = signatureCount == null ? 0 : signatureCount;
+    this.transports =
+        transports == null
+            ? Collections.emptySortedSet()
+            : CollectionUtil.immutableSortedSet(transports);
     this.warnings = CollectionUtil.immutableList(warnings);
     this.attestationMetadata = attestationMetadata;
   }
@@ -150,6 +182,12 @@ public class RegistrationResult {
       class Step4 {
         RegistrationResultBuilder publicKeyCose(ByteArray publicKeyCose) {
           return builder.publicKeyCose(publicKeyCose);
+        }
+      }
+
+      class Step5 {
+        RegistrationResultBuilder signatureCount(long signatureCount) {
+          return builder.signatureCount(signatureCount);
         }
       }
     }

test-dependent-projects/java-dep-webauthn-server-core/src/test/java/com/yubico/webauthn/meta/VersionInfoTest.java
@@ -19,7 +19,7 @@ public class VersionInfoTest {
     final Specification spec = versionInfo.getSpecification();
     assertTrue(spec.getLatestVersionUrl().toExternalForm().startsWith("https://"));
     assertTrue(spec.getUrl().toExternalForm().startsWith("https://"));
-    assertTrue(spec.getReleaseDate().isAfter(LocalDate.of(2019, 3, 3)));
+    assertTrue(spec.getReleaseDate().isAfter(LocalDate.of(2021, 2, 24)));
     assertNotNull(spec.getStatus());
   }
 

webauthn-server-core/build.gradle
@@ -54,13 +54,13 @@ jar {
   manifest {
     attributes([
       'Specification-Title': 'Web Authentication: An API for accessing Public Key Credentials',
-      'Specification-Version': 'Level 1 Recommendation 2019-03-04',
+      'Specification-Version': 'Level 2 Proposed Recommendation 2021-02-25',
       'Specification-Vendor': 'World Wide Web Consortium',
 
-      'Specification-Url': 'https://www.w3.org/TR/2019/REC-webauthn-1-20190304/',
-      'Specification-Url-Latest': 'https://www.w3.org/TR/webauthn/',
-      'Specification-W3c-Status': 'recommendation',
-      'Specification-Release-Date': '2019-03-04',
+      'Specification-Url': 'https://www.w3.org/TR/2021/PR-webauthn-2-20210225/',
+      'Specification-Url-Latest': 'https://www.w3.org/TR/webauthn-2/',
+      'Specification-W3c-Status': 'proposed-recommendation',
+      'Specification-Release-Date': '2021-02-25',
 
       'Implementation-Id': 'java-webauthn-server',
       'Implementation-Title': 'Yubico Web Authentication server library',

webauthn-server-core/src/main/java/com/yubico/webauthn/data/AuthenticatorAttestationResponse.java
@@ -27,8 +27,12 @@ package com.yubico.webauthn.data;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yubico.internal.util.CollectionUtil;
 import com.yubico.webauthn.data.exception.Base64UrlException;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.SortedSet;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
@@ -68,6 +72,15 @@ public class AuthenticatorAttestationResponse implements AuthenticatorResponse {
   @Getter(onMethod = @__({@Override}))
   private final ByteArray clientDataJSON;
 
+  /**
+   * The return value from the <code>AuthenticatorAttestationResponse.getTransports()</code> method.
+   *
+   * @see <a
+   *     href="https://www.w3.org/TR/2021/REC-webauthn-2-20210408/#dom-authenticatorattestationresponse-gettransports">§5.2.1.
+   *     Information About Public Key Credential (interface AuthenticatorAttestationResponse)</a>
+   */
+  private final SortedSet<AuthenticatorTransport> transports;
+
   /** The {@link #attestationObject} parsed as a domain object. */
   @NonNull @JsonIgnore private final transient AttestationObject attestation;
 
@@ -86,10 +99,15 @@ public class AuthenticatorAttestationResponse implements AuthenticatorResponse {
   @JsonCreator
   private AuthenticatorAttestationResponse(
       @NonNull @JsonProperty("attestationObject") ByteArray attestationObject,
-      @NonNull @JsonProperty("clientDataJSON") ByteArray clientDataJSON)
+      @NonNull @JsonProperty("clientDataJSON") ByteArray clientDataJSON,
+      @JsonProperty("transports") Set<AuthenticatorTransport> transports)
       throws IOException, Base64UrlException {
     this.attestationObject = attestationObject;
     this.clientDataJSON = clientDataJSON;
+    this.transports =
+        transports == null
+            ? Collections.emptySortedSet()
+            : CollectionUtil.immutableSortedSet(transports);
 
     attestation = new AttestationObject(attestationObject);
     this.clientData = new CollectedClientData(clientDataJSON);

webauthn-server-core/src/main/java/com/yubico/webauthn/data/PublicKeyCredentialDescriptor.java
@@ -28,6 +28,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yubico.internal.util.CollectionUtil;
 import com.yubico.internal.util.ComparableUtil;
+import com.yubico.webauthn.RegistrationResult;
 import java.util.Optional;
 import java.util.Set;
 import java.util.SortedSet;
@@ -60,6 +61,9 @@ public class PublicKeyCredentialDescriptor implements Comparable<PublicKeyCreden
   /**
    * An OPTIONAL hint as to how the client might communicate with the managing authenticator of the
    * public key credential the caller is referring to.
+   *
+   * <p>This SHOULD be set to the unmodified value returned from {@link
+   * RegistrationResult#getTransports()} when the credential was registered.
    */
   private final SortedSet<AuthenticatorTransport> transports;
 
@@ -119,6 +123,9 @@ public class PublicKeyCredentialDescriptor implements Comparable<PublicKeyCreden
     /**
      * An OPTIONAL hint as to how the client might communicate with the managing authenticator of
      * the public key credential the caller is referring to.
+     *
+     * <p>This SHOULD be set to the unmodified value returned from {@link
+     * RegistrationResult#getTransports()} when the credential was registered.
      */
     public PublicKeyCredentialDescriptorBuilder transports(
         @NonNull Optional<Set<AuthenticatorTransport>> transports) {
@@ -128,6 +135,9 @@ public class PublicKeyCredentialDescriptor implements Comparable<PublicKeyCreden
     /**
      * An OPTIONAL hint as to how the client might communicate with the managing authenticator of
      * the public key credential the caller is referring to.
+     *
+     * <p>This SHOULD be set to the unmodified value returned from {@link
+     * RegistrationResult#getTransports()} when the credential was registered.
      */
     public PublicKeyCredentialDescriptorBuilder transports(Set<AuthenticatorTransport> transports) {
       this.transports = transports;

commit afab2b531044ffe6b9b8cb650601c4b66d1447f0

Fix typo in RegistrationResult.getSignatureCount() javadoc
This was broken by commit dc91acc80b4b822c6b0a00ba50313f358c664a09.

webauthn-server-core/src/main/java/com/yubico/webauthn/RegistrationResult.java
@@ -134,7 +134,7 @@ public class RegistrationResult {
    * <p>This is used in {@link RelyingParty#finishAssertion(FinishAssertionOptions)} to verify the
    * validity of future signature counter values.
    *
-   * @see RegisteredCredential#getSignatureCount() ()
+   * @see RegisteredCredential#getSignatureCount()
    */
   private final long signatureCount;
 