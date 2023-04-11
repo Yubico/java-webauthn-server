fbe533b3aab9d7fe3afdccfc440f8c103aca9eca
Emil Lundberg <emil@yubico.com>
2023-04-11 17:46:38 +0200

    Use Java 8 toolchain to ensure backwards compatibility

--- buildSrc/src/main/kotlin/project-convention-java.gradle.kts
+++ buildSrc/src/main/kotlin/project-convention-java.gradle.kts
@@ -3,18 +3,13 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 tasks.withType(JavaCompile::class) {
     options.compilerArgs.add("-Xlint:deprecation")
     options.compilerArgs.add("-Xlint:unchecked")
     options.encoding = "UTF-8"
-}
-
-tasks.withType(Test::class) {
-    javaLauncher.set(javaToolchains.launcherFor {
-        languageVersion.set(JavaLanguageVersion.of(8))
-    })
 }
--- buildSrc/src/main/kotlin/project-convention-scala.gradle.kts
+++ buildSrc/src/main/kotlin/project-convention-scala.gradle.kts
@@ -3,5 +3,9 @@ plugins {
 }
 
 tasks.withType(ScalaCompile::class) {
-    scalaCompileOptions.additionalParameters = listOf("-Wunused")
+    // listOf doesn't work at the moment
+    // See: https://github.com/gradle/gradle/issues/23193
+    // See: https://github.com/gradle/gradle/pull/23198
+    // See: https://github.com/gradle/gradle/pull/23751
+    scalaCompileOptions.additionalParameters = mutableListOf("-Wunused")
 }
