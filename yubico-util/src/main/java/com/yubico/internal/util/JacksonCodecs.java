5d1effca2b3ce9312ec23c4e8b4b2acbfdad25d1
Emil Lundberg <emil@yubico.com>
2023-04-12 15:09:55 +0200

    Fix incompatibility with Jackson >= 2.15.0-rc1

--- yubico-util/src/main/java/com/yubico/internal/util/JacksonCodecs.java
+++ yubico-util/src/main/java/com/yubico/internal/util/JacksonCodecs.java
@@ -3,6 +3,7 @@ package com.yubico.internal.util;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.Base64Variants;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -22,6 +23,7 @@ public class JacksonCodecs {
     return new ObjectMapper()
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
         .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+        .configure(MapperFeature.PROPAGATE_TRANSIENT_MARKER, true)
         .setSerializationInclusion(Include.NON_ABSENT)
         .setBase64Variant(Base64Variants.MODIFIED_FOR_URL)
         .registerModule(new Jdk8Module())
