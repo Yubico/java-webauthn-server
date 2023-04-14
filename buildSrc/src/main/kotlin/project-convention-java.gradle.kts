b796b95ea5d9648c46b20ed244d1f078b956fc49
Emil Lundberg <emil@yubico.com>
2023-04-14 20:09:12 +0200

    Restore setting to run tests in Java 8

--- buildSrc/src/main/kotlin/project-convention-java.gradle.kts
+++ buildSrc/src/main/kotlin/project-convention-java.gradle.kts
@@ -13,3 +13,9 @@ tasks.withType(JavaCompile::class) {
     options.compilerArgs.add("-Xlint:unchecked")
     options.encoding = "UTF-8"
 }
+
+tasks.withType(Test::class) {
+    javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    })
+}
