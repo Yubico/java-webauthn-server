74dd7483173707f56625ef1fcdeb071d59a93e4c
Emil Lundberg <emil@yubico.com>
2023-05-03 12:45:40 +0200

    Move all version constraints declarations to version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,13 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("constraintLibs") {
             val jacksonVer = version("jackson", "[2.13.2.1,3)")
+
+            library("cbor", "com.upokecenter:cbor:[4.5.1,5)")
+            library("cose", "com.augustcellars.cose:cose-java:[1.0.0,2)")
+            library("guava", "com.google.guava:guava:[24.1.1,32)")
+            library("httpclient5", "org.apache.httpcomponents.client5:httpclient5:[5.0.0,6)")
             library("jackson-bom", "com.fasterxml.jackson", "jackson-bom").versionRef(jacksonVer)
+            library("slf4j", "org.slf4j:slf4j-api:[1.7.25,3)")
         }
     }
 }
