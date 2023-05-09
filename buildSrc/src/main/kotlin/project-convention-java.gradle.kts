fa2b55669b760a45026567e8966dfad50991124f
Emil Lundberg <emil@yubico.com>
2023-05-09 17:59:16 +0200

    Set release or target/sourceCompatibility conditionally

--- buildSrc/src/main/kotlin/project-convention-java.gradle.kts
+++ buildSrc/src/main/kotlin/project-convention-java.gradle.kts
@@ -13,7 +13,13 @@ tasks.withType(JavaCompile::class) {
     options.compilerArgs.add("-Xlint:deprecation")
     options.compilerArgs.add("-Xlint:unchecked")
     options.encoding = "UTF-8"
-    options.release.set(8)
+
+    if (JavaVersion.current().isJava9Compatible) {
+        options.release.set(8)
+    } else {
+        targetCompatibility = "1.8"
+        sourceCompatibility = "1.8"
+    }
 }
 
 tasks.withType(Test::class) {
