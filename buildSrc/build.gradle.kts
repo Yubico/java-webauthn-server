2d643145e110c5b04a0a663dfc3e8af0c436a274
Emil Lundberg <emil@yubico.com>
2025-10-20 14:25:23 +0200

    Don't apply code formatting plugins if Java version < 17
    spotless-plugin-gradle dropped Java <17 support in version 8.0.0.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,8 @@ dependencies {
   implementation("io.franzbecker:gradle-lombok:5.0.0")
 
   // Spotless dropped Java 8 support in version 2.33.0
-  if (JavaVersion.current().isJava11Compatible) {
+  // spotless-plugin-gradle dropped Java <17 support in version 8.0.0
+  if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.0.0")
     implementation("io.github.cosmicsilence:gradle-scalafix:0.2.2")
   }
