cdc9dafff7f3b80397660a1a0d653156ffc91c2e
Emil Lundberg <emil@yubico.com>
2025-12-01 16:05:09 +0100

    fixup! fixup! WIP: Upgrade Gradle wrapper to Gradle 9

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -18,6 +18,6 @@ dependencies {
   // spotless-plugin-gradle dropped Java <17 support in version 8.0.0
   if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.1.0")
-    implementation("io.github.cosmicsilence:gradle-scalafix:0.2.2")
+    implementation("io.github.cosmicsilence:gradle-scalafix:0.2.6")
   }
 }
