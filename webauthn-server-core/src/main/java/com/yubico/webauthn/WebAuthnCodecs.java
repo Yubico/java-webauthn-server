7b6216b4f500e045c027f3666088d0691266cae0
Dennis Fokin <dennis.fokin@yubico.com>
2026-09-17 14:33:38 +0200

    Run spotless

--- webauthn-server-core/src/main/java/com/yubico/webauthn/WebAuthnCodecs.java
+++ webauthn-server-core/src/main/java/com/yubico/webauthn/WebAuthnCodecs.java
@@ -254,8 +254,7 @@ final class WebAuthnCodecs {
 
   private static void validateKtyMatchesAlg(int kty, CBORObject alg) {
     if (alg == null) {
-      throw new IllegalArgumentException(
-          "COSE key is missing required \"alg\" (3) attribute");
+      throw new IllegalArgumentException("COSE key is missing required \"alg\" (3) attribute");
     }
     Optional<COSEAlgorithmIdentifier> algId = COSEAlgorithmIdentifier.fromId(alg.AsInt32());
     if (!algId.isPresent()) {
